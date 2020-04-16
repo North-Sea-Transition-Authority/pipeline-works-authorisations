5123836f316c52be7e3bed94d3e5cb8e2f489166
Cade Ayres <cade.ayres@fivium.co.uk>
2020-04-16 13:43:14 +0100

    Added minimal cable crossing controller, and cable crossing files table.

---
+++ src/main/java/uk/co/ogauthority/pwa/controller/pwaapplications/shared/crossings/CableCrossingController.java
@@ -0,0 +1,51 @@
+package uk.co.ogauthority.pwa.controller.pwaapplications.shared.crossings;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+import uk.co.ogauthority.pwa.controller.pwaapplications.shared.PwaApplicationPermissionCheck;
+import uk.co.ogauthority.pwa.controller.pwaapplications.shared.PwaApplicationStatusCheck;
+import uk.co.ogauthority.pwa.controller.pwaapplications.shared.PwaApplicationTypeCheck;
+import uk.co.ogauthority.pwa.model.form.pwaapplications.shared.crossings.EditBlockCrossingForm;
+import uk.co.ogauthority.pwa.service.enums.pwaapplications.PwaApplicationPermission;
+import uk.co.ogauthority.pwa.service.enums.pwaapplications.PwaApplicationStatus;
+import uk.co.ogauthority.pwa.service.enums.pwaapplications.PwaApplicationType;
+import uk.co.ogauthority.pwa.service.pwaapplications.ApplicationBreadcrumbService;
+import uk.co.ogauthority.pwa.service.pwaapplications.context.PwaApplicationContext;
+import uk.co.ogauthority.pwa.util.converters.ApplicationTypeUrl;
+
+@Controller
+@RequestMapping("/pwa-application/{applicationType}/{applicationId}/crossings")
+@PwaApplicationTypeCheck(types = {
+    PwaApplicationType.INITIAL,
+    PwaApplicationType.CAT_1_VARIATION,
+    PwaApplicationType.CAT_2_VARIATION,
+    PwaApplicationType.DEPOSIT_CONSENT
+})
+@PwaApplicationStatusCheck(status = PwaApplicationStatus.DRAFT)
+@PwaApplicationPermissionCheck(permissions = {PwaApplicationPermission.EDIT})
+public class CableCrossingController {
+
+  private final ApplicationBreadcrumbService applicationBreadcrumbService;
+
+  public CableCrossingController(
+      ApplicationBreadcrumbService applicationBreadcrumbService) {
+    this.applicationBreadcrumbService = applicationBreadcrumbService;
+  }
+
+  @GetMapping
+  @PwaApplicationStatusCheck(status = PwaApplicationStatus.DRAFT)
+  @PwaApplicationPermissionCheck(permissions = {PwaApplicationPermission.EDIT})
+  public ModelAndView renderCableCrossings(
+      @PathVariable("applicationType") @ApplicationTypeUrl PwaApplicationType applicationType,
+      @PathVariable("applicationId") Integer applicationId,
+      @PathVariable("blockCrossingId") Integer blockCrossingId,
+      @ModelAttribute("form") EditBlockCrossingForm form,
+      PwaApplicationContext applicationContext) {
+    return null;
+  }
+
+}
