8151ad1320905c548d50ce0c201c67c04554cafe
Cade Ayres <cade.ayres@fivium.co.uk>
2020-04-28 11:54:04 +0100

    Controller and empty migration

---
+++ src/main/java/uk/co/ogauthority/pwa/controller/pwaapplications/shared/crossings/PipelineCrossingController.java
@@ -0,0 +1,26 @@
+package uk.co.ogauthority.pwa.controller.pwaapplications.shared.crossings;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import uk.co.ogauthority.pwa.controller.pwaapplications.shared.PwaApplicationPermissionCheck;
+import uk.co.ogauthority.pwa.controller.pwaapplications.shared.PwaApplicationStatusCheck;
+import uk.co.ogauthority.pwa.controller.pwaapplications.shared.PwaApplicationTypeCheck;
+import uk.co.ogauthority.pwa.service.enums.pwaapplications.PwaApplicationPermission;
+import uk.co.ogauthority.pwa.service.enums.pwaapplications.PwaApplicationStatus;
+import uk.co.ogauthority.pwa.service.enums.pwaapplications.PwaApplicationType;
+
+@Controller
+@RequestMapping("/pwa-application/{applicationType}/{applicationId}/crossings/pipeline")
+@PwaApplicationTypeCheck(types = {
+    PwaApplicationType.INITIAL,
+    PwaApplicationType.CAT_1_VARIATION,
+    PwaApplicationType.CAT_2_VARIATION,
+    PwaApplicationType.DEPOSIT_CONSENT
+})
+@PwaApplicationStatusCheck(status = PwaApplicationStatus.DRAFT)
+@PwaApplicationPermissionCheck(permissions = {PwaApplicationPermission.EDIT})
+public class PipelineCrossingController {
+
+
+
+}
