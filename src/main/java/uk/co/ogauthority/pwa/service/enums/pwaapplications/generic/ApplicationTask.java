32e5220419286c11e81baf1d7f2ba476a46f8802
Cade Ayres <cade.ayres@fivium.co.uk>
2020-04-28 11:19:04 +0100

    Fixed merge

--- src/main/java/uk/co/ogauthority/pwa/service/enums/pwaapplications/generic/ApplicationTask.java
+++ src/main/java/uk/co/ogauthority/pwa/service/enums/pwaapplications/generic/ApplicationTask.java
@@ -4,7 +4,6 @@ import java.util.stream.Stream;
 import uk.co.ogauthority.pwa.controller.pwaapplications.shared.EnvironmentalDecomController;
 import uk.co.ogauthority.pwa.controller.pwaapplications.shared.FastTrackController;
 import uk.co.ogauthority.pwa.controller.pwaapplications.shared.HuooController;
-import uk.co.ogauthority.pwa.controller.pwaapplications.shared.LocationDetailsController;
 import uk.co.ogauthority.pwa.controller.pwaapplications.shared.ProjectInformationController;
 import uk.co.ogauthority.pwa.controller.pwaapplications.shared.crossings.CrossingAgreementsController;
 import uk.co.ogauthority.pwa.controller.pwaapplications.shared.location.LocationDetailsController;
