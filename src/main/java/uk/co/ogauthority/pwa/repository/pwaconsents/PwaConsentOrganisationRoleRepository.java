357f42e2720f0f1d6d5ab18e4053bad4f1625a34
mheyes <michael.heyes@fivium.co.uk>
2020-06-10 14:39:35 +0100

    fix n+1 on picking pwa for variation

--- src/main/java/uk/co/ogauthority/pwa/repository/pwaconsents/PwaConsentOrganisationRoleRepository.java
+++ src/main/java/uk/co/ogauthority/pwa/repository/pwaconsents/PwaConsentOrganisationRoleRepository.java
@@ -11,12 +11,12 @@ import uk.co.ogauthority.pwa.model.entity.pwaconsents.PwaConsentOrganisationRole
 
 public interface PwaConsentOrganisationRoleRepository extends CrudRepository<PwaConsentOrganisationRole, Integer> {
 
-  @EntityGraph(attributePaths = {"addedByPwaConsent", "endedByPwaConsent"})
+  @EntityGraph(attributePaths = {"addedByPwaConsent", "addedByPwaConsent.masterPwa"})
   List<PwaConsentOrganisationRole> findByOrganisationUnitIdInAndRoleInAndEndTimestampIsNull(
       Collection<Integer> organisationUnitIds,
       Collection<HuooRole> roles);
 
-  @EntityGraph(attributePaths = {"addedByPwaConsent", "endedByPwaConsent"})
+  @EntityGraph(attributePaths = {"addedByPwaConsent", "addedByPwaConsent.masterPwa"})
   List<PwaConsentOrganisationRole> findByAddedByPwaConsentInAndRoleInAndEndTimestampIsNull(
       Collection<PwaConsent> pwaConsents,
       Collection<HuooRole> roles);
