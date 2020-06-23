46f5e9027cedc3a7ca7691a25010a75cdffe3cc0
Cade Ayres <cade.ayres@fivium.co.uk>
2020-06-23 10:26:42 +0100

    Renamed get view tests

--- src/test/java/uk/co/ogauthority/pwa/service/pwaapplications/shared/pipelines/PadBundleServiceTest.java
+++ src/test/java/uk/co/ogauthority/pwa/service/pwaapplications/shared/pipelines/PadBundleServiceTest.java
@@ -72,7 +72,7 @@ public class PadBundleServiceTest {
   }
 
   @Test
-  public void getBundleViews() {
+  public void getBundleViews_whenLinksExist() {
     var bundle = new PadBundle();
     bundle.setBundleName("name");
 
@@ -92,7 +92,7 @@ public class PadBundleServiceTest {
   }
 
   @Test
-  public void getBundleSummaryViews() {
+  public void getBundleSummaryViews_whenLinksExists() {
     var bundle = new PadBundle();
     bundle.setBundleName("name");
 
