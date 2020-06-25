47e089ba1fd9579e298604b9310b7e90a9740187
Michael Heyes <michael.heyes@fivium.co.uk>
2020-06-25 14:40:09 +0000

    Merged in feature/patch-fix (pull request #186)
    Reset patch 65 to original merge with develop
    
    * Reset patch 65 to original merge with develop
    
    Approved-by: Chris Wood
    Approved-by: Daleel Ramjan

--- src/main/resources/db/migration/V65__fluid_composition_table.sql
+++ src/main/resources/db/migration/V65__fluid_composition_table.sql
@@ -1,7 +1,7 @@
 CREATE TABLE ${datasource.user}.pad_fluid_composition_info (
    id NUMBER GENERATED ALWAYS AS IDENTITY PRIMARY KEY
     , application_detail_id NUMBER NOT NULL
-    , chemical_name VARCHAR2(50)
+    , chemical_name VARCHAR2(4000) NOT NULL
     , fluid_composition_option VARCHAR2(50)
     , mole_value NUMBER
     , CONSTRAINT pad_fluidci_pad_fk FOREIGN KEY(application_detail_id) REFERENCES ${datasource.user}.pwa_application_details(id)
