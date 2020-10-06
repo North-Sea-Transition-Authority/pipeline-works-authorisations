54c6793c6768710ef0b8ae4b029f4f3ff025546d
Chris Wood <chris.wood@fivium.co.uk>
2020-10-06 08:12:41 +0000

    Merged in feature/PWA-879/consultee-view-fix (pull request #400)
    Ensure view only looks at same app versions as the base app search view
    
    Approved-by: Michael Heyes <michael.heyes@fivium.co.uk>

--- src/main/resources/db/migration/R__0070_consultation_request_search_item_view.sql
+++ src/main/resources/db/migration/R__0070_consultation_request_search_item_view.sql
@@ -10,6 +10,14 @@ SELECT
 , ca.assignee_name assigned_responder_name
 FROM ${datasource.user}.consultation_requests cr
 JOIN ${datasource.user}.pwa_applications pa ON pa.id = cr.application_id
-JOIN ${datasource.user}.pwa_application_details pad ON pad.pwa_application_id = pa.id AND pad.tip_flag = 1
+JOIN ${datasource.user}.pwa_application_details pad ON pad.pwa_application_id = pa.id
+JOIN ${datasource.user}.pad_status_versions psv ON pa.id = psv.pwa_application_id
 JOIN ${datasource.user}.consultee_group_details cgd ON cgd.cg_id = cr.consultee_group_id AND cgd.tip_flag = 1
-LEFT JOIN ${datasource.user}.consultation_assignments ca ON ca.consultation_request_id = cr.id AND ca.assignment = 'CONSULTATION_RESPONDER';
+LEFT JOIN ${datasource.user}.consultation_assignments ca ON ca.consultation_request_id = cr.id AND ca.assignment = 'CONSULTATION_RESPONDER'
+WHERE pad.version_no = CASE
+  -- if a submitted version exists, always return the last submitted
+  WHEN psv.last_submitted_version IS NOT NULL THEN psv.last_submitted_version
+  -- else if the detail is the current draft version return that version.
+  WHEN psv.max_draft_version = pad.version_no THEN psv.max_draft_version
+  ELSE NULL
+END;
