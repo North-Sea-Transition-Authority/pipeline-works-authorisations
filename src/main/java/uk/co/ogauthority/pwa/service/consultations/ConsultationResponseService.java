19d067fda38b817009348ba913d04065e0e6a4f1
Chris Wood <chris.wood@fivium.co.uk>
2020-10-16 11:03:32 +0000

    Merged in feature/PWA-837/email-co-after-consultation-response (pull request #422)
    Email assigned CO when consultee responds
    
    Update tests
    
    Approved-by: Daleel Ramjan <daleel.ramjan@fivium.co.uk>
    Approved-by: Michael Heyes <michael.heyes@fivium.co.uk>

--- src/main/java/uk/co/ogauthority/pwa/service/consultations/ConsultationResponseService.java
+++ src/main/java/uk/co/ogauthority/pwa/service/consultations/ConsultationResponseService.java
@@ -12,14 +12,21 @@ import org.springframework.stereotype.Service;
 import org.springframework.validation.BindingResult;
 import uk.co.ogauthority.pwa.energyportal.model.entity.PersonId;
 import uk.co.ogauthority.pwa.energyportal.model.entity.WebUserAccount;
+import uk.co.ogauthority.pwa.exception.WorkflowAssignmentException;
 import uk.co.ogauthority.pwa.model.entity.consultations.ConsultationRequest;
 import uk.co.ogauthority.pwa.model.entity.consultations.ConsultationResponse;
 import uk.co.ogauthority.pwa.model.form.consultation.ConsultationResponseForm;
 import uk.co.ogauthority.pwa.model.form.enums.ConsultationResponseOption;
+import uk.co.ogauthority.pwa.model.notify.emailproperties.ConsultationResponseReceivedEmailProps;
 import uk.co.ogauthority.pwa.repository.consultations.ConsultationResponseRepository;
+import uk.co.ogauthority.pwa.service.appprocessing.consultations.consultees.ConsulteeGroupDetailService;
 import uk.co.ogauthority.pwa.service.enums.pwaapplications.ConsultationRequestStatus;
 import uk.co.ogauthority.pwa.service.enums.workflow.PwaApplicationConsultationWorkflowTask;
+import uk.co.ogauthority.pwa.service.enums.workflow.PwaApplicationWorkflowTask;
+import uk.co.ogauthority.pwa.service.notify.EmailCaseLinkService;
+import uk.co.ogauthority.pwa.service.notify.NotifyService;
 import uk.co.ogauthority.pwa.service.workflow.CamundaWorkflowService;
+import uk.co.ogauthority.pwa.service.workflow.assignment.WorkflowAssignmentService;
 import uk.co.ogauthority.pwa.service.workflow.task.WorkflowTaskInstance;
 import uk.co.ogauthority.pwa.validators.consultations.ConsultationResponseValidator;
 
@@ -34,6 +41,10 @@ public class ConsultationResponseService {
   private final ConsultationResponseValidator consultationResponseValidator;
   private final CamundaWorkflowService camundaWorkflowService;
   private final Clock clock;
+  private final NotifyService notifyService;
+  private final ConsulteeGroupDetailService consulteeGroupDetailService;
+  private final WorkflowAssignmentService workflowAssignmentService;
+  private final EmailCaseLinkService emailCaseLinkService;
 
   @Autowired
   public ConsultationResponseService(
@@ -41,12 +52,20 @@ public class ConsultationResponseService {
       ConsultationResponseRepository consultationResponseRepository,
       ConsultationResponseValidator consultationResponseValidator,
       CamundaWorkflowService camundaWorkflowService,
-      @Qualifier("utcClock") Clock clock) {
+      @Qualifier("utcClock") Clock clock,
+      NotifyService notifyService,
+      ConsulteeGroupDetailService consulteeGroupDetailService,
+      WorkflowAssignmentService workflowAssignmentService,
+      EmailCaseLinkService emailCaseLinkService) {
     this.consultationRequestService = consultationRequestService;
     this.consultationResponseRepository = consultationResponseRepository;
     this.consultationResponseValidator = consultationResponseValidator;
     this.camundaWorkflowService = camundaWorkflowService;
     this.clock = clock;
+    this.notifyService = notifyService;
+    this.consulteeGroupDetailService = consulteeGroupDetailService;
+    this.workflowAssignmentService = workflowAssignmentService;
+    this.emailCaseLinkService = emailCaseLinkService;
   }
 
 
@@ -86,24 +105,49 @@ public class ConsultationResponseService {
 
   @Transactional
   public void saveResponseAndCompleteWorkflow(ConsultationResponseForm form, ConsultationRequest consultationRequest, WebUserAccount user) {
+
     ConsultationResponse consultationResponse = mapFormToResponse(form, consultationRequest, user);
     consultationResponseRepository.save(consultationResponse);
     camundaWorkflowService.completeTask(new WorkflowTaskInstance(consultationRequest, PwaApplicationConsultationWorkflowTask.RESPONSE));
     consultationRequest.setStatus(ConsultationRequestStatus.RESPONDED);
     consultationRequestService.saveConsultationRequest(consultationRequest);
-  }
 
+    sendResponseNotificationToCaseOfficer(consultationRequest, consultationResponse);
+
+  }
 
   public BindingResult validate(ConsultationResponseForm form, BindingResult bindingResult) {
     consultationResponseValidator.validate(form, bindingResult);
     return bindingResult;
   }
 
+  private void sendResponseNotificationToCaseOfficer(ConsultationRequest consultationRequest, ConsultationResponse response) {
 
+    var application = consultationRequest.getPwaApplication();
 
+    var caseOfficerPerson = workflowAssignmentService
+        .getAssignee(new WorkflowTaskInstance(application, PwaApplicationWorkflowTask.CASE_OFFICER_REVIEW))
+        .orElseThrow(() -> new WorkflowAssignmentException(
+            String.format("Expected to find a case officer for application with ID: %s", application.getId())));
 
+    String caseOfficerName = caseOfficerPerson.getFullName();
+    String caseOfficerEmail = caseOfficerPerson.getEmailAddress();
 
+    var tipGroupDetail = consulteeGroupDetailService
+        .getConsulteeGroupDetailByGroupAndTipFlagIsTrue(consultationRequest.getConsulteeGroup());
 
+    String consulteeGroupName = tipGroupDetail.getName();
 
+    var emailProps = new ConsultationResponseReceivedEmailProps(
+        caseOfficerName,
+        application.getAppReference(),
+        consulteeGroupName,
+        response.getResponseType().getDisplayText(),
+        emailCaseLinkService.generateCaseManagementLink(application)
+    );
+
+    notifyService.sendEmail(emailProps, caseOfficerEmail);
+
+  }
 
 }
