ecd4d16891678e37533486bc5924e2e3afbeb9e9
mheyes <michael.heyes@fivium.co.uk>
2020-11-02 19:12:44 +0000

    fix idiocy

--- src/main/resources/db/migration/V102__add_response_columns_to_app_update_request.sql
+++ src/main/resources/db/migration/V102__add_response_columns_to_app_update_request.sql
@@ -1,12 +1,12 @@
 
-ALTER TABLE pwa_mh.application_update_requests ADD (
+ALTER TABLE ${datasource.user}.application_update_requests ADD (
   response_by_person_id NUMBER
 , response_timestamp TIMESTAMP
 , response_pad_id NUMBER
 , response_other_changes VARCHAR2(4000)
 );
 
-ALTER TABLE pwa_mh.application_update_requests ADD (
+ALTER TABLE ${datasource.user}.application_update_requests ADD (
  CONSTRAINT aur_response_ck CHECK (
     (response_by_person_id IS NULL AND response_timestamp IS NULL AND response_pad_id IS NULL)
   OR
