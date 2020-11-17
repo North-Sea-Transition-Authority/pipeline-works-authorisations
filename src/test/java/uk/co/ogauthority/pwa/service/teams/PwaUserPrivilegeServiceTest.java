cf013c62672b2f8060ba78b861950d164326ad5c
mheyes <michael.heyes@fivium.co.uk>
2020-11-17 16:22:35 +0000

    Allow users in only the PwaContact teams access to the work area

--- src/test/java/uk/co/ogauthority/pwa/service/teams/PwaUserPrivilegeServiceTest.java
+++ src/test/java/uk/co/ogauthority/pwa/service/teams/PwaUserPrivilegeServiceTest.java
@@ -3,6 +3,7 @@ package uk.co.ogauthority.pwa.service.teams;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.when;
 
+import java.util.EnumSet;
 import java.util.Optional;
 import java.util.Set;
 import org.junit.Before;
@@ -16,6 +17,9 @@ import uk.co.ogauthority.pwa.model.entity.appprocessing.consultations.consultees
 import uk.co.ogauthority.pwa.model.entity.appprocessing.consultations.consultees.ConsulteeGroupMemberRole;
 import uk.co.ogauthority.pwa.model.entity.appprocessing.consultations.consultees.ConsulteeGroupTeamMember;
 import uk.co.ogauthority.pwa.service.appprocessing.consultations.consultees.ConsulteeGroupTeamService;
+import uk.co.ogauthority.pwa.service.enums.masterpwas.contacts.PwaContactRole;
+import uk.co.ogauthority.pwa.service.pwaapplications.contacts.PwaApplicationContactRoleDto;
+import uk.co.ogauthority.pwa.service.pwaapplications.contacts.PwaContactService;
 import uk.co.ogauthority.pwa.testutils.ConsulteeGroupTestingUtils;
 
 @RunWith(MockitoJUnitRunner.class)
@@ -24,15 +28,19 @@ public class PwaUserPrivilegeServiceTest {
   @Mock
   private ConsulteeGroupTeamService groupTeamService;
 
+  @Mock
+  private PwaContactService pwaContactService;
+
   private PwaUserPrivilegeService userPrivilegeService;
 
   private Person person = new Person(1, null, null, null, null);
 
-  private ConsulteeGroupDetail groupDetail = ConsulteeGroupTestingUtils.createConsulteeGroup("Environmental Management Team", "EMT");
+  private ConsulteeGroupDetail groupDetail = ConsulteeGroupTestingUtils.createConsulteeGroup(
+      "Environmental Management Team", "EMT");
 
   @Before
   public void setUp() {
-    userPrivilegeService = new PwaUserPrivilegeService(groupTeamService);
+    userPrivilegeService = new PwaUserPrivilegeService(groupTeamService, pwaContactService);
   }
 
   @Test
@@ -49,7 +57,8 @@ public class PwaUserPrivilegeServiceTest {
   @Test
   public void getPwaUserPrivilegesForPerson_consulteeGroupMember_notAccessManager() {
 
-    var member = new ConsulteeGroupTeamMember(groupDetail.getConsulteeGroup(), person, Set.of(ConsulteeGroupMemberRole.RECIPIENT));
+    var member = new ConsulteeGroupTeamMember(groupDetail.getConsulteeGroup(), person,
+        Set.of(ConsulteeGroupMemberRole.RECIPIENT));
 
     when(groupTeamService.getTeamMemberByPerson(person)).thenReturn(Optional.of(member));
 
@@ -62,7 +71,8 @@ public class PwaUserPrivilegeServiceTest {
   @Test
   public void getPwaUserPrivilegesForPerson_consulteeGroupMember_isAccessManager() {
 
-    var member = new ConsulteeGroupTeamMember(groupDetail.getConsulteeGroup(), person, Set.of(ConsulteeGroupMemberRole.ACCESS_MANAGER));
+    var member = new ConsulteeGroupTeamMember(groupDetail.getConsulteeGroup(), person,
+        Set.of(ConsulteeGroupMemberRole.ACCESS_MANAGER));
 
     when(groupTeamService.getTeamMemberByPerson(person)).thenReturn(Optional.of(member));
 
@@ -71,6 +81,57 @@ public class PwaUserPrivilegeServiceTest {
     assertThat(privSet).containsExactlyInAnyOrder(
         PwaUserPrivilege.PWA_WORKAREA,
         PwaUserPrivilege.PWA_CONSULTEE_GROUP_ADMIN,
+        PwaUserPrivilege.PWA_CONSULTEE);
+
+  }
+
+
+  @Test
+  public void getPwaUserPrivilegesForPerson_pwaContact_isInAnyContactTeamRole() {
+
+    var contactRoleDto = new PwaApplicationContactRoleDto(person.getId().asInt(), 1, PwaContactRole.PREPARER);
+
+    when(pwaContactService.getPwaContactRolesForPerson(person, EnumSet.allOf(PwaContactRole.class)))
+        .thenReturn(Set.of(contactRoleDto));
+
+    var privSet = userPrivilegeService.getPwaUserPrivilegesForPerson(person);
+
+    assertThat(privSet).containsExactlyInAnyOrder(
+        PwaUserPrivilege.PWA_WORKAREA,
+        PwaUserPrivilege.PWA_INDUSTRY
+    );
+
+  }
+
+  @Test
+  public void getPwaUserPrivilegesForPerson_pwaContact_isNotInAnyContactTeamRole() {
+
+    when(pwaContactService.getPwaContactRolesForPerson(person, EnumSet.allOf(PwaContactRole.class)))
+        .thenReturn(Set.of());
+
+    var privSet = userPrivilegeService.getPwaUserPrivilegesForPerson(person);
+
+    assertThat(privSet).isEmpty();
+
+  }
+
+  @Test
+  public void getPwaUserPrivilegesForPerson_pwaContact_and_consultee() {
+
+    var contactRoleDto = new PwaApplicationContactRoleDto(person.getId().asInt(), 1, PwaContactRole.PREPARER);
+    when(pwaContactService.getPwaContactRolesForPerson(person, EnumSet.allOf(PwaContactRole.class)))
+        .thenReturn(Set.of(contactRoleDto));
+
+    var member = new ConsulteeGroupTeamMember(groupDetail.getConsulteeGroup(), person,
+        Set.of(ConsulteeGroupMemberRole.RECIPIENT));
+    when(groupTeamService.getTeamMemberByPerson(person)).thenReturn(Optional.of(member));
+
+
+    var privSet = userPrivilegeService.getPwaUserPrivilegesForPerson(person);
+
+    assertThat(privSet).containsExactlyInAnyOrder(
+        PwaUserPrivilege.PWA_WORKAREA,
+        PwaUserPrivilege.PWA_INDUSTRY,
         PwaUserPrivilege.PWA_CONSULTEE);
 
   }
