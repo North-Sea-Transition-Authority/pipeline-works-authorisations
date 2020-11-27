103400639a285d8cdf61552fdbfa6a201698171c
mheyes <michael.heyes@fivium.co.uk>
2020-11-27 15:39:25 +0000

    Options approval confirmation submission service.

---
+++ src/main/java/uk/co/ogauthority/pwa/service/pwaapplications/workflow/PwaApplicationOptionConfirmationSubmissionService.java
@@ -0,0 +1,62 @@
+package uk.co.ogauthority.pwa.service.pwaapplications.workflow;
+
+import java.util.Optional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.lang.Nullable;
+import org.springframework.stereotype.Service;
+import uk.co.ogauthority.pwa.energyportal.model.entity.Person;
+import uk.co.ogauthority.pwa.model.entity.pwaapplications.PwaApplicationDetail;
+import uk.co.ogauthority.pwa.service.appprocessing.ApplicationInvolvementService;
+import uk.co.ogauthority.pwa.service.enums.pwaapplications.PwaApplicationStatus;
+import uk.co.ogauthority.pwa.service.enums.workflow.PwaApplicationSubmitResult;
+import uk.co.ogauthority.pwa.service.enums.workflow.PwaApplicationWorkflowTask;
+
+/**
+ * Service to to customise submission behaviour when an update request is responded to.
+ */
+@Service
+class PwaApplicationOptionConfirmationSubmissionService implements ApplicationSubmissionService {
+
+  private final ApplicationInvolvementService applicationInvolvementService;
+
+  @Autowired
+  public PwaApplicationOptionConfirmationSubmissionService(ApplicationInvolvementService applicationInvolvementService) {
+
+    this.applicationInvolvementService = applicationInvolvementService;
+  }
+
+  @Override
+  public Optional<PwaApplicationSubmitResult> getSubmissionWorkflowResult() {
+    return Optional.empty();
+  }
+
+  @Override
+  public PwaApplicationWorkflowTask getTaskToComplete() {
+    return PwaApplicationWorkflowTask.UPDATE_APPLICATION;
+  }
+
+  @Override
+  public PwaApplicationStatus getSubmittedApplicationDetailStatus(PwaApplicationDetail pwaApplicationDetail) {
+    var caseOfficerPersonIdOpt = applicationInvolvementService.getCaseOfficerPersonId(
+        pwaApplicationDetail.getPwaApplication());
+
+    if (caseOfficerPersonIdOpt.isPresent()) {
+      return PwaApplicationStatus.CASE_OFFICER_REVIEW;
+    }
+
+    return PwaApplicationStatus.INITIAL_SUBMISSION_REVIEW;
+
+  }
+
+  @Override
+  public void doBeforeSubmit(PwaApplicationDetail pwaApplicationDetail, Person submittedByPerson,
+                             @Nullable String submissionDescription) {
+    // nothing to do here
+  }
+
+  @Override
+  public void doAfterSubmit(PwaApplicationDetail pwaApplicationDetail) {
+    // nothing to do here
+  }
+
+}
