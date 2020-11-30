cb4ae3ccef6a56afe74afcb173156f3d1f8e6d97
Chris Wood <chris.wood@fivium.co.uk>
2020-11-30 11:24:04 +0000

    Merged in feature/PWA-101/confirm-satisfactory-case-history (pull request #515)
    Add case history items for app confirm satisfactory events
    
    * Confirm satisfactory application task functionality
    
    * Update/add tests
    
    * Lock down required tasks unless there has been at least one app version confirmed satisfactory
        Update tests
    
    * Add case history items for app confirm satisfactory events
        Add test
    
    * Fix date test
    
    Approved-by: Daleel Ramjan <daleel.ramjan@fivium.co.uk>
    Approved-by: Michael Heyes <michael.heyes@fivium.co.uk>

---
+++ src/test/java/uk/co/ogauthority/pwa/service/appprocessing/casehistory/ConfirmSatisfactoryApplicationCaseHistoryItemServiceTest.java
@@ -0,0 +1,92 @@
+package uk.co.ogauthority.pwa.service.appprocessing.casehistory;
+
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.when;
+
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
+import java.util.List;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+import uk.co.ogauthority.pwa.energyportal.model.entity.PersonId;
+import uk.co.ogauthority.pwa.model.entity.pwaapplications.PwaApplication;
+import uk.co.ogauthority.pwa.model.entity.pwaapplications.PwaApplicationDetail;
+import uk.co.ogauthority.pwa.model.view.appprocessing.casehistory.CaseHistoryItemView;
+import uk.co.ogauthority.pwa.service.enums.pwaapplications.PwaApplicationType;
+import uk.co.ogauthority.pwa.service.pwaapplications.PwaApplicationDetailService;
+import uk.co.ogauthority.pwa.testutils.PwaApplicationTestUtil;
+
+@RunWith(MockitoJUnitRunner.class)
+public class ConfirmSatisfactoryApplicationCaseHistoryItemServiceTest {
+
+  private final static PersonId PERSON_ID1 = new PersonId(1);
+  private final static PersonId PERSON_ID2 = new PersonId(2);
+
+  private final static int APP_ID = 10;
+  private final static int APP_DETAIL_1_ID = 1;
+  private final static int APP_DETAIL_2_ID = 2;
+
+  private final static Instant APP_DETAIL_1_CONFIRMED = Instant.now().truncatedTo(ChronoUnit.DAYS);
+  private final static Instant APP_DETAIL_2_CONFIRMED = Instant.now()
+      .truncatedTo(ChronoUnit.DAYS)
+      .plus(1, ChronoUnit.HOURS);
+
+  @Mock
+  private PwaApplicationDetailService pwaApplicationDetailService;
+
+  private ConfirmSatisfactoryApplicationCaseHistoryItemService confirmSatisfactoryApplicationCaseHistoryItemService;
+
+  private PwaApplicationDetail detail1;
+  private PwaApplicationDetail detail2;
+  private PwaApplicationDetail ignoredDetail;
+  private PwaApplication application;
+
+
+  @Before
+  public void setUp() throws Exception {
+
+    confirmSatisfactoryApplicationCaseHistoryItemService = new ConfirmSatisfactoryApplicationCaseHistoryItemService(pwaApplicationDetailService);
+
+    detail1 = PwaApplicationTestUtil.createDefaultApplicationDetail(PwaApplicationType.INITIAL, APP_ID, APP_DETAIL_1_ID, 1);
+    detail1.setConfirmedSatisfactoryTimestamp(APP_DETAIL_1_CONFIRMED);
+    detail1.setConfirmedSatisfactoryByPersonId(PERSON_ID1);
+
+    detail2 = PwaApplicationTestUtil.createDefaultApplicationDetail(PwaApplicationType.INITIAL, APP_ID, APP_DETAIL_2_ID, 2);
+    detail2.setConfirmedSatisfactoryTimestamp(APP_DETAIL_2_CONFIRMED);
+    detail2.setConfirmedSatisfactoryByPersonId(PERSON_ID2);
+    detail2.setConfirmedSatisfactoryReason("reason");
+
+    ignoredDetail = PwaApplicationTestUtil.createDefaultApplicationDetail(PwaApplicationType.INITIAL, APP_ID, 5, 3);
+
+    application = detail1.getPwaApplication();
+    detail2.setPwaApplication(application);
+    ignoredDetail.setPwaApplication(application);
+
+  }
+
+  @Test
+  public void getCaseHistoryItemViews_whenMultipleVersionsConfirmed_andNotAllVersionsConfirmed() {
+
+    when(pwaApplicationDetailService.getAllSubmittedApplicationDetailsForApplication(application))
+        .thenReturn(List.of(detail1, detail2, ignoredDetail));
+
+    var historyItems = confirmSatisfactoryApplicationCaseHistoryItemService.getCaseHistoryItemViews(application);
+
+    assertThat(historyItems).containsExactly(
+        new CaseHistoryItemView.Builder("Application version 1 confirmed satisfactory", APP_DETAIL_1_CONFIRMED, PERSON_ID1)
+            .setPersonLabelText("Confirmed by")
+            .setPersonEmailLabel("Contact email")
+            .build(),
+        new CaseHistoryItemView.Builder("Application version 2 confirmed satisfactory", APP_DETAIL_2_CONFIRMED, PERSON_ID2)
+            .setPersonLabelText("Confirmed by")
+            .setPersonEmailLabel("Contact email")
+            .addDataItem("Reasons for confirmation", detail2.getConfirmedSatisfactoryReason())
+            .build()
+    );
+
+  }
+}
