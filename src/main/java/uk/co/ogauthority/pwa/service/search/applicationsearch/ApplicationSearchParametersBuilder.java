6f91d848167dd804d0be34b8d20164cef43becd4
mheyes <michael.heyes@fivium.co.uk>
2021-02-03 19:39:46 +0000

    Include completed/withdrawn applications filter

--- src/main/java/uk/co/ogauthority/pwa/service/search/applicationsearch/ApplicationSearchParametersBuilder.java
+++ src/main/java/uk/co/ogauthority/pwa/service/search/applicationsearch/ApplicationSearchParametersBuilder.java
@@ -3,16 +3,25 @@ package uk.co.ogauthority.pwa.service.search.applicationsearch;
 public class ApplicationSearchParametersBuilder {
   private String appReference;
 
+  private Boolean includeCompletedOrWithdrawnApps;
+
   public ApplicationSearchParametersBuilder setAppReference(String appReference) {
     this.appReference = appReference;
     return this;
   }
 
+  public ApplicationSearchParametersBuilder includeCompletedOrWithdrawnApps(Boolean includeCompletedOrWithdrawnApps) {
+    this.includeCompletedOrWithdrawnApps = includeCompletedOrWithdrawnApps;
+    return this;
+  }
+
   public ApplicationSearchParameters createApplicationSearchParameters() {
-    return new ApplicationSearchParameters(appReference);
+    return new ApplicationSearchParameters(appReference, includeCompletedOrWithdrawnApps);
   }
 
   public static ApplicationSearchParameters createEmptyParams() {
-    return new ApplicationSearchParameters(null);
+    return new ApplicationSearchParameters(
+        null, null
+    );
   }
 }
