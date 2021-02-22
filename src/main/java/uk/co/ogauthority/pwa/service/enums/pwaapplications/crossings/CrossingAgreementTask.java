441ff201eff74c20eb5f375fdd473ad16acf571f
Michael Heyes <michael.heyes@fivium.co.uk>
2021-02-22 12:13:40 +0000

    Merged in feature/PWA-1119/hide-irrelavant-crossing-info-on-summary (pull request #647)
    Feature/PWA-1119/hide irrelavant crossing info on summary
    
    * Hide pipeline crossing summary section when not relevant
    
    * Hide cable crossing summary section when not relevant
    
    * Hide median line crossing summary section when not relevant
    
    * Use ApplicationTaskService and GeneralPurposeTask interface to control showing of crossing types ofn summary. This avoids duplicating the app type business rules.
    
    * Throw exception if unexpected interaction with task adapter methods attempted.
    
    Approved-by: Chris Wood
    Approved-by: Daleel Ramjan

--- src/main/java/uk/co/ogauthority/pwa/service/enums/pwaapplications/crossings/CrossingAgreementTask.java
+++ src/main/java/uk/co/ogauthority/pwa/service/enums/pwaapplications/crossings/CrossingAgreementTask.java
@@ -2,6 +2,11 @@ package uk.co.ogauthority.pwa.service.enums.pwaapplications.crossings;
 
 import java.util.Arrays;
 import java.util.stream.Stream;
+import uk.co.ogauthority.pwa.controller.pwaapplications.shared.crossings.BlockCrossingController;
+import uk.co.ogauthority.pwa.controller.pwaapplications.shared.crossings.CableCrossingController;
+import uk.co.ogauthority.pwa.controller.pwaapplications.shared.crossings.CrossingTypesController;
+import uk.co.ogauthority.pwa.controller.pwaapplications.shared.crossings.MedianLineCrossingController;
+import uk.co.ogauthority.pwa.controller.pwaapplications.shared.crossings.PipelineCrossingController;
 import uk.co.ogauthority.pwa.service.pwaapplications.generic.ApplicationFormSectionService;
 import uk.co.ogauthority.pwa.service.pwaapplications.shared.crossings.BlockCrossingService;
 import uk.co.ogauthority.pwa.service.pwaapplications.shared.crossings.CrossingTypesService;
@@ -11,20 +16,28 @@ import uk.co.ogauthority.pwa.service.pwaapplications.shared.crossings.pipeline.P
 
 public enum CrossingAgreementTask {
 
-  LICENCE_AND_BLOCKS("Licence and blocks", BlockCrossingService.class, 10),
-  CROSSING_TYPES("Types of crossing", CrossingTypesService.class, 20),
-  PIPELINE_CROSSINGS("Pipeline crossings", PadPipelineCrossingService.class, 30),
-  CABLE_CROSSINGS("Cable crossings", PadCableCrossingService.class, 40),
-  MEDIAN_LINE("Median line crossing", PadMedianLineAgreementService.class, 50);
+  LICENCE_AND_BLOCKS("Licence and blocks", BlockCrossingService.class, BlockCrossingController.class, 10),
+  CROSSING_TYPES("Types of crossing", CrossingTypesService.class, CrossingTypesController.class, 20),
+  PIPELINE_CROSSINGS("Pipeline crossings", PadPipelineCrossingService.class, PipelineCrossingController.class, 30),
+  CABLE_CROSSINGS("Cable crossings", PadCableCrossingService.class, CableCrossingController.class, 40),
+  MEDIAN_LINE("Median line crossing", PadMedianLineAgreementService.class, MedianLineCrossingController.class, 50);
 
   private final String displayText;
   private final Class<? extends ApplicationFormSectionService> sectionClass;
+  private final Class<?> controllerClass;
   private final int displayOrder;
 
-  CrossingAgreementTask(String displayText, Class<? extends ApplicationFormSectionService> sectionClass, int displayOrder) {
+  CrossingAgreementTask(String displayText,
+                        Class<? extends ApplicationFormSectionService> sectionClass,
+                        Class<?> controllerClass, int displayOrder) {
     this.displayText = displayText;
     this.sectionClass = sectionClass;
+    this.controllerClass = controllerClass;
     this.displayOrder = displayOrder;
+  }
+
+  public Class<?> getControllerClass() {
+    return controllerClass;
   }
 
   public String getDisplayText() {
