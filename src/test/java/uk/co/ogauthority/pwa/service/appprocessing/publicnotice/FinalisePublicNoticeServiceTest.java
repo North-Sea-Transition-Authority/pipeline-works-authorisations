7d2711691b4b366ec397691d11699228217af8e7
Daleel <daleel.ramjan@fivium.co.uk>
2021-03-12 16:15:25 +0000

    fixed test bad import

--- src/test/java/uk/co/ogauthority/pwa/service/appprocessing/publicnotice/FinalisePublicNoticeServiceTest.java
+++ src/test/java/uk/co/ogauthority/pwa/service/appprocessing/publicnotice/FinalisePublicNoticeServiceTest.java
@@ -29,7 +29,7 @@ import uk.co.ogauthority.pwa.repository.publicnotice.PublicNoticeDatesRepository
 import uk.co.ogauthority.pwa.service.enums.masterpwas.contacts.PwaContactRole;
 import uk.co.ogauthority.pwa.service.enums.pwaapplications.PwaApplicationType;
 import uk.co.ogauthority.pwa.service.enums.workflow.PwaApplicationPublicNoticeFinalisationResult;
-import uk.co.ogauthority.pwa.service.enums.workflow.PwaApplicationPublicNoticeWorkflowTask;
+import uk.co.ogauthority.pwa.service.enums.workflow.publicnotice.PwaApplicationPublicNoticeWorkflowTask;
 import uk.co.ogauthority.pwa.service.notify.EmailCaseLinkService;
 import uk.co.ogauthority.pwa.service.notify.NotifyService;
 import uk.co.ogauthority.pwa.service.pwaapplications.contacts.PwaContactService;
