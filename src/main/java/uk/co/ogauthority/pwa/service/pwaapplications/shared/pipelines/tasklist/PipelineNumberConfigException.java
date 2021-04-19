ef00254fc09a8e53a1984c370fd97fa1d6effeee
mheyes <michael.heyes@fivium.co.uk>
2021-04-19 13:50:15 +0100

    fix checkstyle

--- src/main/java/uk/co/ogauthority/pwa/service/pwaapplications/shared/pipelines/tasklist/PipelineNumberConfigException.java
+++ src/main/java/uk/co/ogauthority/pwa/service/pwaapplications/shared/pipelines/tasklist/PipelineNumberConfigException.java
@@ -4,7 +4,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ResponseStatus(value = HttpStatus.INTERNAL_SERVER_ERROR, reason = "Problem retrieving pipeline number config")
-public class PipelineNumberConfigException extends RuntimeException{
+public class PipelineNumberConfigException extends RuntimeException {
 
   PipelineNumberConfigException(String message) {
     super(message);
