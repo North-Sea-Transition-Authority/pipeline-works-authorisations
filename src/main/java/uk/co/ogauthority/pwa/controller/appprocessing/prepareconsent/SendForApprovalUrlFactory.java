f02116793bdb22edbfc626b138546b1563fa2f8e
mheyes <michael.heyes@fivium.co.uk>
2021-05-10 15:40:53 +0100

    Add link to app summary on parallel consents view. Fix bug when no public notice exists but user in holder team.

---
+++ src/main/java/uk/co/ogauthority/pwa/controller/appprocessing/prepareconsent/SendForApprovalUrlFactory.java
@@ -0,0 +1,25 @@
+package uk.co.ogauthority.pwa.controller.appprocessing.prepareconsent;
+
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+import uk.co.ogauthority.pwa.service.appprocessing.prepareconsent.ParallelConsentView;
+import uk.co.ogauthority.pwa.util.CaseManagementUtils;
+
+public class SendForApprovalUrlFactory {
+
+  private Map<Integer, String> applicationAppSummaryUrls;
+
+  public SendForApprovalUrlFactory(List<ParallelConsentView> parallelConsentViewList) {
+    this.applicationAppSummaryUrls = parallelConsentViewList
+        .stream()
+        .collect(Collectors.toMap(
+            ParallelConsentView::getPwaApplicationId,
+            o -> CaseManagementUtils.routeApplicationSummary(o.getPwaApplicationId(), o.getPwaApplicationType())
+        ));
+  }
+
+  public String getAppSummaryUrlByAppId(int appId){
+    return this.applicationAppSummaryUrls.get(appId);
+  }
+}
