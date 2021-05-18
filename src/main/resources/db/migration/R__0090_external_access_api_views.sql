ecc74b62f43aba74c09236a3410ca5109b055894
mheyes <michael.heyes@fivium.co.uk>
2021-05-18 17:41:46 +0100

    Fix view script that flyway doesnt like

--- src/main/resources/db/migration/R__0090_external_access_api_views.sql
+++ src/main/resources/db/migration/R__0090_external_access_api_views.sql
@@ -1,70 +1,73 @@
 CREATE OR REPLACE VIEW ${datasource.user}.api_vw_pwa_pipeline_details AS
-WITH pipeline_types AS (
--- flattened version PipelineType
-SELECT 'UNKNOWN' type_mnem, 'Unknown pipeline type' type_display, 'SINGLE_CORE' core_type
-FROM DUAL
-UNION ALL
-SELECT 'PRODUCTION_FLOWLINE' type_mnem, 'Production Flowline' type_display, 'SINGLE_CORE' core_type
-FROM dual
-UNION ALL
-SELECT 'PRODUCTION_JUMPER' type_mnem, 'Production Jumper' type_display, 'SINGLE_CORE' core_type
-FROM dual
-UNION ALL
-SELECT 'GAS_LIFT_PIPELINE' type_mnem, 'Gas Lift Pipeline' type_display, 'SINGLE_CORE' core_type
-FROM dual
-UNION ALL
-SELECT 'GAS_LIFT_JUMPER' type_mnem, 'Gas Lift Jumper' type_display, 'SINGLE_CORE' core_type
-FROM dual
-UNION ALL
-SELECT 'WATER_INJECTION_PIPELINE' type_mnem, 'Water Injection Pipeline' type_display, 'SINGLE_CORE' core_type
-FROM dual
-UNION ALL
-SELECT 'WATER_INJECTION_JUMPER' type_mnem, 'Water Injection Jumper' type_display, 'SINGLE_CORE' core_type
-FROM dual
-UNION ALL
-SELECT 'METHANOL_PIPELINE' type_mnem, 'Methanol Pipeline' type_display, 'SINGLE_CORE' core_type
-FROM dual
-UNION ALL
-SELECT 'SERVICES_UMBILICAL' type_mnem, 'Services Umbilical' type_display, 'MULTI_CORE' core_type
-FROM dual
-UNION ALL
-SELECT 'HYDRAULIC_JUMPER' type_mnem, 'Hydraulic Jumper' type_display, 'MULTI_CORE' core_type
-FROM dual
-UNION ALL
-SELECT 'CHEMICAL_JUMPER' type_mnem, 'Chemical Jumper' type_display, 'SINGLE_CORE' core_type
-FROM dual
-UNION ALL
-SELECT 'CONTROL_JUMPER' type_mnem, 'Control Jumper' type_display, 'SINGLE_CORE' core_type
-FROM dual
-UNION ALL
-SELECT 'UMBILICAL_JUMPER' type_mnem, 'Umbilical Jumper' type_display, 'MULTI_CORE' core_type
-FROM dual
-UNION ALL
-SELECT 'CABLE' type_mnem, 'Cable' type_display, 'SINGLE_CORE' core_type
-FROM dual
-)  
-SELECT
-  pd.id pipeline_detail_id
-, pd.pipeline_id
-, pd.pwa_consent_id
-, CAST(pd.start_timestamp AS DATE) start_date
-, CAST(pd.end_timestamp AS DATE) end_date
-, CASE WHEN pd.tip_flag = 1 THEN 'C' END status_control
-, pd.pipeline_number
-, pd.pipeline_status -- PENDING, DELETED, NEVER_LAID, TRANSFERRED can all be ignored generally
-, pd.from_location
-, pd.to_location
-, pd.max_external_diameter
-, COALESCE(pdmd.commissioned_date, NULL) commissioned_date -- we do not map commissioned date in new system
-, pd.pipeline_number || ' -' ||
-  CASE
-    WHEN pt.core_type = 'SINGLE_CORE' AND pd.max_external_diameter IS NOT NULL THEN ' ' || pd.max_external_diameter || ' Millimetre'
-  END || ' ' || pt.type_display ||
-  CASE WHEN pd.pipeline_in_bundle  = 1 THEN ' (' || pd.bundle_name || ')'
-  END pipeline_name
-FROM ${datasource.user}.pipeline_details pd
-LEFT JOIN ${datasource.user}.pipeline_detail_migration_data pdmd ON pdmd.pipeline_detail_id = pd.id
-JOIN pipeline_types pt ON COALESCE(pd.pipeline_type, 'UNKNOWN') = pt.type_mnem;
+SELECT *
+FROM (
+  WITH pipeline_types AS (
+  -- flattened version PipelineType
+  SELECT 'UNKNOWN' type_mnem, 'Unknown pipeline type' type_display, 'SINGLE_CORE' core_type
+  FROM DUAL
+  UNION ALL
+  SELECT 'PRODUCTION_FLOWLINE' type_mnem, 'Production Flowline' type_display, 'SINGLE_CORE' core_type
+  FROM dual
+  UNION ALL
+  SELECT 'PRODUCTION_JUMPER' type_mnem, 'Production Jumper' type_display, 'SINGLE_CORE' core_type
+  FROM dual
+  UNION ALL
+  SELECT 'GAS_LIFT_PIPELINE' type_mnem, 'Gas Lift Pipeline' type_display, 'SINGLE_CORE' core_type
+  FROM dual
+  UNION ALL
+  SELECT 'GAS_LIFT_JUMPER' type_mnem, 'Gas Lift Jumper' type_display, 'SINGLE_CORE' core_type
+  FROM dual
+  UNION ALL
+  SELECT 'WATER_INJECTION_PIPELINE' type_mnem, 'Water Injection Pipeline' type_display, 'SINGLE_CORE' core_type
+  FROM dual
+  UNION ALL
+  SELECT 'WATER_INJECTION_JUMPER' type_mnem, 'Water Injection Jumper' type_display, 'SINGLE_CORE' core_type
+  FROM dual
+  UNION ALL
+  SELECT 'METHANOL_PIPELINE' type_mnem, 'Methanol Pipeline' type_display, 'SINGLE_CORE' core_type
+  FROM dual
+  UNION ALL
+  SELECT 'SERVICES_UMBILICAL' type_mnem, 'Services Umbilical' type_display, 'MULTI_CORE' core_type
+  FROM dual
+  UNION ALL
+  SELECT 'HYDRAULIC_JUMPER' type_mnem, 'Hydraulic Jumper' type_display, 'MULTI_CORE' core_type
+  FROM dual
+  UNION ALL
+  SELECT 'CHEMICAL_JUMPER' type_mnem, 'Chemical Jumper' type_display, 'SINGLE_CORE' core_type
+  FROM dual
+  UNION ALL
+  SELECT 'CONTROL_JUMPER' type_mnem, 'Control Jumper' type_display, 'SINGLE_CORE' core_type
+  FROM dual
+  UNION ALL
+  SELECT 'UMBILICAL_JUMPER' type_mnem, 'Umbilical Jumper' type_display, 'MULTI_CORE' core_type
+  FROM dual
+  UNION ALL
+  SELECT 'CABLE' type_mnem, 'Cable' type_display, 'SINGLE_CORE' core_type
+  FROM dual
+  )
+  SELECT
+    pd.id pipeline_detail_id
+  , pd.pipeline_id
+  , pd.pwa_consent_id
+  , CAST(pd.start_timestamp AS DATE) start_date
+  , CAST(pd.end_timestamp AS DATE) end_date
+  , CASE WHEN pd.tip_flag = 1 THEN 'C' END status_control
+  , pd.pipeline_number
+  , pd.pipeline_status -- PENDING, DELETED, NEVER_LAID, TRANSFERRED can all be ignored generally
+  , pd.from_location
+  , pd.to_location
+  , pd.max_external_diameter
+  , COALESCE(pdmd.commissioned_date, NULL) commissioned_date -- we do not map commissioned date in new system
+  , pd.pipeline_number || ' -' ||
+    CASE
+      WHEN pt.core_type = 'SINGLE_CORE' AND pd.max_external_diameter IS NOT NULL THEN ' ' || pd.max_external_diameter || ' Millimetre'
+    END || ' ' || pt.type_display ||
+    CASE WHEN pd.pipeline_in_bundle  = 1 THEN ' (' || pd.bundle_name || ')'
+    END pipeline_name
+  FROM ${datasource.user}.pipeline_details pd
+  LEFT JOIN ${datasource.user}.pipeline_detail_migration_data pdmd ON pdmd.pipeline_detail_id = pd.id
+  JOIN pipeline_types pt ON COALESCE(pd.pipeline_type, 'UNKNOWN') = pt.type_mnem
+);
 
 CREATE OR REPLACE VIEW ${datasource.user}.api_vw_pwa_consents AS
 SELECT
