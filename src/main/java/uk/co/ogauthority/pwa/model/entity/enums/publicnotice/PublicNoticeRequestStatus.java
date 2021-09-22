abe97cf3bbf82bc0924e800e2d724191d5c130fe
Zubaer Rahman <zubaer.rahman@fivium.co.uk>
2021-09-22 15:49:29 +0000

    Merged in feature/PWA-1351/show-public-notice-rejection (pull request #1004)
    PWA-1351: added request status and rejection reason to public notice summary
    
    * PWA-1351: added request status and rejection reason to public notice summary
    
    
    Approved-by: Chris Wood
    Approved-by: Daleel Ramjan

--- src/main/java/uk/co/ogauthority/pwa/model/entity/enums/publicnotice/PublicNoticeRequestStatus.java
+++ src/main/java/uk/co/ogauthority/pwa/model/entity/enums/publicnotice/PublicNoticeRequestStatus.java
@@ -2,8 +2,18 @@ package uk.co.ogauthority.pwa.model.entity.enums.publicnotice;
 
 public enum PublicNoticeRequestStatus {
 
-  WAITING_MANAGER_APPROVAL,
-  REJECTED,
-  APPROVED;
+  WAITING_MANAGER_APPROVAL("Waiting for manager approval"),
+  REJECTED("Rejected"),
+  APPROVED("Approved");
+
+  private final String displayText;
+
+  PublicNoticeRequestStatus(String displayText) {
+    this.displayText = displayText;
+  }
+
+  public String getDisplayText() {
+    return displayText;
+  }
 
 }
