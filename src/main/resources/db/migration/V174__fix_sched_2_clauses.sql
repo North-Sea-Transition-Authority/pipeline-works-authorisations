701c5fb1f832439f019327e4a70641cbaf25b810
Chris Wood <chris.wood@fivium.co.uk>
2021-10-01 07:46:49 +0000

    Merged in feature/PWA-1471/fix-multiple-tip-versions-on-sched2-clauses (pull request #1025)
    Fix patched clauses to have their own clause ids, correct parent
    
    * Fix patched clauses to have their own clause ids, correct parent
    
    
    Approved-by: Daleel Ramjan
    Approved-by: Michael Heyes

---
+++ src/main/resources/db/migration/V174__fix_sched_2_clauses.sql
@@ -0,0 +1,54 @@
+DECLARE
+
+    l_sched2_s_id NUMBER;
+    l_new_sc_id NUMBER;
+    l_new_parent_sc_id NUMBER;
+
+BEGIN
+
+    SELECT s.id
+    INTO l_sched2_s_id
+    FROM ${datasource.user}.dt_sections s
+    WHERE s.name = 'SCHEDULE_2';
+
+    SELECT s.sc_id
+    INTO l_new_parent_sc_id
+    FROM ${datasource.user}.dt_section_clause_versions s
+    WHERE s.name = 'On Completion of Construction/Installation'
+    AND s.tip_flag = 1
+    AND s.version_no = 1;
+
+    INSERT INTO ${datasource.user}.dt_section_clauses (id, s_id) values (${datasource.user}.dt_section_clauses_id_seq.nextval, l_sched2_s_id)
+    RETURNING id INTO l_new_sc_id;
+
+    UPDATE ${datasource.user}.dt_section_clause_versions
+    SET
+        sc_id = l_new_sc_id
+      , parent_sc_id = l_new_parent_sc_id
+    WHERE name = 'Written Notification'
+    AND tip_flag = 1
+    AND version_no = 1;
+
+    INSERT INTO ${datasource.user}.dt_section_clauses (id, s_id) values (${datasource.user}.dt_section_clauses_id_seq.nextval, l_sched2_s_id)
+    RETURNING id INTO l_new_sc_id;
+
+    UPDATE ${datasource.user}.dt_section_clause_versions
+    SET
+        sc_id = l_new_sc_id
+      , parent_sc_id = l_new_parent_sc_id
+    WHERE name = 'Copies of drawings'
+    AND tip_flag = 1
+    AND version_no = 1;
+
+    INSERT INTO ${datasource.user}.dt_section_clauses (id, s_id) values (${datasource.user}.dt_section_clauses_id_seq.nextval, l_sched2_s_id)
+    RETURNING id INTO l_new_sc_id;
+
+    UPDATE ${datasource.user}.dt_section_clause_versions
+    SET
+        sc_id = l_new_sc_id
+      , parent_sc_id = l_new_parent_sc_id
+    WHERE name = 'Co-ordinates'
+    AND tip_flag = 1
+    AND version_no = 1;
+
+END;
