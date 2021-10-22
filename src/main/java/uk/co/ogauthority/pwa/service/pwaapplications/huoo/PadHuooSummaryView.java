d119fa0836fa1c714763a72255a4c32c4961c4fd
mheyes <michael.heyes@fivium.co.uk>
2021-10-22 21:44:26 +0100

    Refactor PadOrganisationRole service into seperate services for huoo summary validation, huoo summary view processing, and general case org role reporting

---
+++ src/main/java/uk/co/ogauthority/pwa/service/pwaapplications/huoo/PadHuooSummaryView.java
@@ -0,0 +1,37 @@
+package uk.co.ogauthority.pwa.service.pwaapplications.huoo;
+
+import java.util.List;
+import uk.co.ogauthority.pwa.model.form.pwaapplications.views.HuooOrganisationUnitRoleView;
+import uk.co.ogauthority.pwa.model.form.pwaapplications.views.HuooTreatyAgreementView;
+
+/**
+ * View object used to support the Huoo Summary task screen.
+ */
+public final class PadHuooSummaryView {
+
+  private final List<HuooOrganisationUnitRoleView> huooOrganisationUnitRoleViews;
+
+  private final List<HuooTreatyAgreementView> treatyAgreementViews;
+
+  private final boolean canShowHolderGuidance;
+
+  public PadHuooSummaryView(
+      List<HuooOrganisationUnitRoleView> huooOrganisationUnitRoleViews,
+      List<HuooTreatyAgreementView> treatyAgreementViews, boolean canShowHolderGuidance) {
+    this.huooOrganisationUnitRoleViews = huooOrganisationUnitRoleViews;
+    this.treatyAgreementViews = treatyAgreementViews;
+    this.canShowHolderGuidance = canShowHolderGuidance;
+  }
+
+  public List<HuooOrganisationUnitRoleView> getHuooOrganisationUnitRoleViews() {
+    return huooOrganisationUnitRoleViews;
+  }
+
+  public List<HuooTreatyAgreementView> getTreatyAgreementViews() {
+    return treatyAgreementViews;
+  }
+
+  public boolean canShowHolderGuidance() {
+    return canShowHolderGuidance;
+  }
+}
