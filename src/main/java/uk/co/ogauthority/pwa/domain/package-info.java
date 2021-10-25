2c885431a4f81de13d5029de99be1c0daaf0e1bd
mheyes <michael.heyes@fivium.co.uk>
2021-10-25 18:49:15 +0100

    Clarify top level domain package documentation comments.

--- src/main/java/uk/co/ogauthority/pwa/domain/package-info.java
+++ src/main/java/uk/co/ogauthority/pwa/domain/package-info.java
@@ -2,14 +2,14 @@
 /**
  * <p>This module is containing objects and services for core business logic concepts.</p>
  *
- * <p>This module contains code that is going to be of key to implementing many application use cases.</p>
+ * <p>This module contains code that is going to be key to implementing many application use cases.</p>
  *
- * <p>Functionality that exists to support a single screen of single use case does not belong here.</p>
+ * <p>Functionality that exists to support a single screen or single use case does not belong here.</p>
  *
  * <p>Nothing include this package should refer to anything outside (in an upper level).
- * That means no(or very very limited) Spring framework code,or importing any use case specific code.</p>
+ * That means no(or very very limited) Spring framework code, Hibernate specific code, or simply application use case specific code.</p>
  *
  * <p>This package exists to support a "vertical slice" architecture (with the goal to move to a "package by component"
- * style) where certain domain critical logic has been split following out due its general purpose applicability.</p>
+ * style) where certain domain critical logic has been seperated out due its general purpose applicability.</p>
  */
 package uk.co.ogauthority.pwa.domain;
