eb6aee3182a18b0c2736228eb2566e10702ffa75
mheyes <michael.heyes@fivium.co.uk>
2021-10-25 13:26:47 +0100

    Add package stubs and documentation for critical application domain objects

---
+++ src/main/java/uk/co/ogauthority/pwa/domain/energyportal/organisations/model/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Hold core objects that represent the "Organisation" domain concepts.
+ */
+package uk.co.ogauthority.pwa.domain.energyportal.organisations.model;
---
+++ src/main/java/uk/co/ogauthority/pwa/domain/pwa/huoo/model/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * This package exists to contains the key objects that represent HUOO concepts across PWA.
+ */
+package uk.co.ogauthority.pwa.domain.pwa.huoo.model;
