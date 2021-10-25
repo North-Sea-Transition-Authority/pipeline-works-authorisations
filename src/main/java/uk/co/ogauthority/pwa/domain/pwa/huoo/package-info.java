380e76ed6a7f7fe230665b1ba3f53e031064b945
mheyes <michael.heyes@fivium.co.uk>
2021-10-25 13:26:47 +0100

    Add package stubs and documentation for critical application domain objects

---
+++ src/main/java/uk/co/ogauthority/pwa/domain/pwa/huoo/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * <p>This module exists to capture code which support HUOO processing thats not specific to individual application use cases.</p>
+ */
+
+package uk.co.ogauthority.pwa.domain.pwa.huoo;
---
+++ src/main/java/uk/co/ogauthority/pwa/domain/pwa/pipeline/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Captures core representation and logic of the Pipeline concept within PWA.
+ */
+package uk.co.ogauthority.pwa.domain.pwa.pipeline;
