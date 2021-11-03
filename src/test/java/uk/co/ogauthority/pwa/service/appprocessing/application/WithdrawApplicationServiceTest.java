7cf738f41425f2ab19238629e72538e155025b3e
Daleel Ramjan <daleel.ramjan@fivium.co.uk>
2021-11-03 08:58:25 +0000

    Merged in bugfix/PWA-1504/applicant-is-able-to-update-app-after-being-withdrawn (pull request #1076)
    PWA-1504: Applicant is able to update the application after being withdrawn
    
    * new methods in detailService to handle last submitted and update requested details, updated withdrawAppService to take care of last submitted and update requeted apps, new status for ended update requests, added tests
    
    * Merge remote-tracking branch 'origin/develop' into bugfix/PWA-1504/applicant-is-able-to-update-app-after-being-withdrawn
    
    * Fix withdraw app test by putting service in correct location.
    
    * updated variable/method naming, better lambda formatting, fixed broken tests
    
    * Merge remote-tracking branch 'origin/bugfix/PWA-1504/applicant-is-able-to-update-app-after-being-withdrawn' into bugfix/PWA-1504/applicant-is-able-to-update-app-after-being-withdrawn
    
    Approved-by: Michael Heyes
    Approved-by: Chris Wood

--- src/test/java/uk/co/ogauthority/pwa/service/appprocessing/application/WithdrawApplicationServiceTest.java
+++ src/test/java/uk/co/ogauthority/pwa/service/appprocessing/application/WithdrawApplicationServiceTest.java
@@ -2,13 +2,16 @@ package uk.co.ogauthority.pwa.service.appprocessing.application;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.atLeastOnce;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.Set;
+import java.util.function.Consumer;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -23,10 +26,12 @@ import uk.co.ogauthority.pwa.energyportal.model.entity.PersonTestUtil;
 import uk.co.ogauthority.pwa.energyportal.model.entity.WebUserAccount;
 import uk.co.ogauthority.pwa.features.application.authorisation.appcontacts.PwaContactRole;
 import uk.co.ogauthority.pwa.features.application.authorisation.appcontacts.PwaContactService;
+import uk.co.ogauthority.pwa.exception.WithdrawApplicationException;
 import uk.co.ogauthority.pwa.model.entity.pwaapplications.PwaApplication;
 import uk.co.ogauthority.pwa.model.entity.pwaapplications.PwaApplicationDetail;
 import uk.co.ogauthority.pwa.model.form.withdraw.WithdrawApplicationForm;
 import uk.co.ogauthority.pwa.model.notify.emailproperties.applicationworkflow.ApplicationWithdrawnEmailProps;
+import uk.co.ogauthority.pwa.service.appprocessing.applicationupdate.ApplicationUpdateRequestService;
 import uk.co.ogauthority.pwa.service.appprocessing.context.PwaAppProcessingContext;
 import uk.co.ogauthority.pwa.service.consultations.WithdrawConsultationService;
 import uk.co.ogauthority.pwa.service.enums.appprocessing.PwaAppProcessingPermission;
@@ -35,6 +40,7 @@ import uk.co.ogauthority.pwa.service.enums.pwaapplications.PwaApplicationType;
 import uk.co.ogauthority.pwa.service.notify.EmailCaseLinkService;
 import uk.co.ogauthority.pwa.service.notify.NotifyService;
 import uk.co.ogauthority.pwa.service.pwaapplications.PwaApplicationDetailService;
+import uk.co.ogauthority.pwa.service.pwaapplications.PwaApplicationService;
 import uk.co.ogauthority.pwa.service.workflow.CamundaWorkflowService;
 import uk.co.ogauthority.pwa.validators.WithdrawApplicationValidator;
 
@@ -46,11 +52,15 @@ public class WithdrawApplicationServiceTest {
   @Mock
   private WithdrawApplicationValidator withdrawApplicationValidator;
   @Mock
+  private PwaApplicationService pwaApplicationService;
+  @Mock
   private PwaApplicationDetailService pwaApplicationDetailService;
   @Mock
   private CamundaWorkflowService camundaWorkflowService;
   @Mock
   private WithdrawConsultationService withdrawConsultationService;
+  @Mock
+  private ApplicationUpdateRequestService applicationUpdateRequestService;
   @Mock
   private NotifyService notifyService;
   @Mock
@@ -66,22 +76,29 @@ public class WithdrawApplicationServiceTest {
   private ArgumentCaptor<Person> personCaptor;
   @Captor
   private ArgumentCaptor<String> withdrawReasonCaptor;
+  @Captor
+  private ArgumentCaptor<Consumer<PwaApplicationDetail>> pwaLastSubmittedDetailConsumerCaptor;
+  @Captor
+  private ArgumentCaptor<Consumer<PwaApplicationDetail>> pwaUpdateRequestedDetailConsumerCaptor;
 
   private PwaApplicationDetail pwaApplicationDetail;
+  private PwaApplication pwaApplication;
 
 
   @Before
   public void setUp() {
     withdrawApplicationService = new WithdrawApplicationService(
         withdrawApplicationValidator,
+        pwaApplicationService,
         pwaApplicationDetailService,
         camundaWorkflowService,
         withdrawConsultationService,
+        applicationUpdateRequestService,
         notifyService,
         pwaContactService,
         emailCaseLinkService);
 
-    var pwaApplication = new PwaApplication(null, PwaApplicationType.INITIAL, null);
+    pwaApplication = new PwaApplication(null, PwaApplicationType.INITIAL, null);
     pwaApplicationDetail = new PwaApplicationDetail(pwaApplication, null, null, null);
   }
 
@@ -107,15 +124,73 @@ public class WithdrawApplicationServiceTest {
         appPerson.getFullName(), pwaApplicationDetail.getPwaApplicationRef(), withdrawingUser.getFullName(),
         caseManagementLink);
 
-    withdrawApplicationService.withdrawApplication(form, pwaApplicationDetail, withdrawingUser);
+    withdrawApplicationService.withdrawApplication(form, pwaApplication, withdrawingUser);
 
-    verify(pwaApplicationDetailService, times(1)).setWithdrawn(pwaApplicationDetail, withdrawingPerson, form.getWithdrawalReason());
     verify(camundaWorkflowService, times(1)).deleteProcessInstanceAndThenTasks(pwaApplicationDetail.getPwaApplication());
     verify(withdrawConsultationService, times(1)).withdrawAllOpenConsultationRequests(pwaApplicationDetail.getPwaApplication(), withdrawingUser);
     verify(notifyService, times(2)).sendEmail(any(), any());
     verify(notifyService, atLeastOnce()).sendEmail(emailProps, appPerson.getEmailAddress());
     verify(notifyService, atLeastOnce()).sendEmail(emailProps, withdrawingPerson.getEmailAddress());
+    verify(pwaApplicationDetailService).doWithLastSubmittedDetailIfExists(eq(pwaApplication), any(Consumer.class));
+    verify(pwaApplicationDetailService).doWithCurrentUpdateRequestedDetailIfExists(eq(pwaApplication), any(Consumer.class));
   }
+
+
+  @Test
+  public void withdrawApplication_verifyLastSubmittedDetailHandlerFunctionCalled_verifyOtherServiceInteractions() {
+
+    var withdrawingPerson = PersonTestUtil.createDefaultPerson();
+    var withdrawingUser = new AuthenticatedUserAccount(new WebUserAccount(1, withdrawingPerson), List.of());
+    var form = new WithdrawApplicationForm();
+    form.setWithdrawalReason("my reason");
+
+    withdrawApplicationService.withdrawApplication(form, pwaApplication, withdrawingUser);
+
+    verify(pwaApplicationDetailService).doWithLastSubmittedDetailIfExists(
+        eq(pwaApplication), pwaLastSubmittedDetailConsumerCaptor.capture());
+
+    pwaLastSubmittedDetailConsumerCaptor.getValue().accept(pwaApplicationDetail);
+    verify(pwaApplicationDetailService).setWithdrawn(pwaApplicationDetail, withdrawingPerson, form.getWithdrawalReason());
+    verify(pwaApplicationDetailService).doWithLastSubmittedDetailIfExists(eq(pwaApplication), any(Consumer.class));
+  }
+
+  @Test
+  public void withdrawApplication_updateRequestExists_verifyUpdateRequestedDetailHandlerFunctionCalled_verifyOtherServiceInteractions() {
+
+    var withdrawingUser = new AuthenticatedUserAccount(new WebUserAccount(1, PersonTestUtil.createDefaultPerson()), List.of());
+    var form = new WithdrawApplicationForm();
+    form.setWithdrawalReason("my reason");
+
+    var lastSubmittedDetail = new PwaApplicationDetail();
+    when(pwaApplicationDetailService.getLatestSubmittedDetail(pwaApplication)).thenReturn(Optional.of(lastSubmittedDetail));
+
+    withdrawApplicationService.withdrawApplication(form, pwaApplication, withdrawingUser);
+
+    verify(pwaApplicationDetailService).doWithCurrentUpdateRequestedDetailIfExists(
+        eq(pwaApplication), pwaUpdateRequestedDetailConsumerCaptor.capture());
+
+    pwaUpdateRequestedDetailConsumerCaptor.getValue().accept(pwaApplicationDetail);
+    verify(applicationUpdateRequestService, times(1)).endUpdateRequestIfExists(pwaApplicationDetail);
+    verify(pwaApplicationDetailService, times(1)).transferTipFlag(pwaApplicationDetail, lastSubmittedDetail);
+  }
+
+  @Test(expected = WithdrawApplicationException.class)
+  public void withdrawApplication_updateRequestExists_lastSubmittedDetailNotFound_error() {
+
+    var withdrawingUser = new AuthenticatedUserAccount(new WebUserAccount(1, PersonTestUtil.createDefaultPerson()), List.of());
+    var form = new WithdrawApplicationForm();
+    form.setWithdrawalReason("my reason");
+
+    when(pwaApplicationDetailService.getLatestSubmittedDetail(pwaApplication)).thenReturn(Optional.empty());
+
+    withdrawApplicationService.withdrawApplication(form, pwaApplication, withdrawingUser);
+
+    verify(pwaApplicationDetailService).doWithCurrentUpdateRequestedDetailIfExists(
+        eq(pwaApplication), pwaUpdateRequestedDetailConsumerCaptor.capture());
+
+    pwaUpdateRequestedDetailConsumerCaptor.getValue().accept(pwaApplicationDetail);
+  }
+
 
   @Test
   public void canShowInTaskList_appNotEnded_hasPermission_true() {
