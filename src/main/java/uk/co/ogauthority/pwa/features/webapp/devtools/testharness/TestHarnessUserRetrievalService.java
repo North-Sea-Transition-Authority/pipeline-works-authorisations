7262f53ac63b49f02c5d92446ff511cc4d380b1d
mheyes <michael.heyes@fivium.co.uk>
2021-11-05 10:36:13 +0000

    create devtools directory to collect misc dev focussed features and remove them from the projects top level.

---
+++ src/main/java/uk/co/ogauthority/pwa/features/webapp/devtools/testharness/TestHarnessUserRetrievalService.java
@@ -0,0 +1,63 @@
+package uk.co.ogauthority.pwa.features.webapp.devtools.testharness;
+
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+import uk.co.ogauthority.pwa.auth.AuthenticatedUserAccount;
+import uk.co.ogauthority.pwa.exception.PwaEntityNotFoundException;
+import uk.co.ogauthority.pwa.integrations.energyportal.people.external.PersonId;
+import uk.co.ogauthority.pwa.integrations.energyportal.people.external.PersonService;
+import uk.co.ogauthority.pwa.integrations.energyportal.webuseraccount.external.WebUserAccount;
+import uk.co.ogauthority.pwa.integrations.energyportal.webuseraccount.external.WebUserAccountStatus;
+import uk.co.ogauthority.pwa.integrations.energyportal.webuseraccount.internal.WebUserAccountRepository;
+import uk.co.ogauthority.pwa.service.teams.TeamService;
+
+@Service
+@Profile("test-harness")
+public class TestHarnessUserRetrievalService {
+
+  private final PersonService personService;
+  private final TeamService teamService;
+  private final WebUserAccountRepository webUserAccountRepository;
+
+
+  @Autowired
+  public TestHarnessUserRetrievalService(
+      PersonService personService,
+      TeamService teamService, WebUserAccountRepository webUserAccountRepository) {
+    this.personService = personService;
+    this.teamService = teamService;
+    this.webUserAccountRepository = webUserAccountRepository;
+  }
+
+
+
+  public WebUserAccount getWebUserAccount(Integer personId) {
+
+    var person = personService.getPersonById(new PersonId(personId));
+
+    return webUserAccountRepository.findAllByPersonAndAccountStatusIn(
+        person, List.of(WebUserAccountStatus.ACTIVE, WebUserAccountStatus.NEW))
+        .stream().findAny().orElseThrow(() -> new PwaEntityNotFoundException(
+            "Could not find web user account for person id: " + personId));
+  }
+
+
+  public AuthenticatedUserAccount createAuthenticatedUserAccount(Integer personId) {
+
+    var webUserAccount = getWebUserAccount(personId);
+    return createAuthenticatedUserAccount(webUserAccount);
+  }
+
+  public AuthenticatedUserAccount createAuthenticatedUserAccount(WebUserAccount webUserAccount) {
+    var userPrivs = teamService.getAllUserPrivilegesForPerson(webUserAccount.getLinkedPerson());
+    return new AuthenticatedUserAccount(webUserAccount, userPrivs);
+  }
+
+
+
+
+
+
+}
---
+++ src/main/java/uk/co/ogauthority/pwa/features/webapp/devtools/testharness/appprocessinggeneration/ConfirmSatisfactoryGeneratorService.java
@@ -0,0 +1,39 @@
+package uk.co.ogauthority.pwa.features.webapp.devtools.testharness.appprocessinggeneration;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+import uk.co.ogauthority.pwa.service.appprocessing.application.ConfirmSatisfactoryApplicationService;
+import uk.co.ogauthority.pwa.service.enums.appprocessing.PwaAppProcessingTask;
+
+@Service
+@Profile("test-harness")
+class ConfirmSatisfactoryGeneratorService implements TestHarnessAppProcessingService {
+
+  private final ConfirmSatisfactoryApplicationService confirmSatisfactoryApplicationService;
+
+  private static final PwaAppProcessingTask LINKED_APP_PROCESSING_TASK = PwaAppProcessingTask.CONFIRM_SATISFACTORY_APPLICATION;
+
+  @Autowired
+  public ConfirmSatisfactoryGeneratorService(
+      ConfirmSatisfactoryApplicationService confirmSatisfactoryApplicationService) {
+    this.confirmSatisfactoryApplicationService = confirmSatisfactoryApplicationService;
+  }
+
+
+  @Override
+  public PwaAppProcessingTask getLinkedAppProcessingTask() {
+    return LINKED_APP_PROCESSING_TASK;
+  }
+
+
+  @Override
+  public void generateAppProcessingTaskData(TestHarnessAppProcessingProperties appProcessingProps) {
+    confirmSatisfactoryApplicationService.confirmSatisfactory(
+        appProcessingProps.getPwaApplicationDetail(),
+        "Confirming test harness application satisfactory reason",
+        appProcessingProps.getCaseOfficerAua().getLinkedPerson());
+  }
+
+
+}
