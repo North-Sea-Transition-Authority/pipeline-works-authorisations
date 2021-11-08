0e9623d2db18e3415573a4cbcfd84d3cbe52b3ae
mheyes <michael.heyes@fivium.co.uk>
2021-11-08 14:44:39 +0000

    Move application submission code into an application/submission feature package.

---
+++ src/main/java/uk/co/ogauthority/pwa/features/application/submission/PadPipelineSubmissionRepository.java
@@ -0,0 +1,9 @@
+package uk.co.ogauthority.pwa.features.application.submission;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+import uk.co.ogauthority.pwa.features.application.tasks.pipelines.core.PadPipeline;
+
+@Repository
+public interface PadPipelineSubmissionRepository extends CrudRepository<PadPipeline, Integer>, PadPipelineNumberingRepository {
+}
--- src/main/java/uk/co/ogauthority/pwa/repository/submission/PadPipelineSubmissionRepository.java
+++
@@ -1,9 +0,0 @@
-package uk.co.ogauthority.pwa.repository.submission;
-
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-import uk.co.ogauthority.pwa.features.application.tasks.pipelines.core.PadPipeline;
-
-@Repository
-public interface PadPipelineSubmissionRepository extends CrudRepository<PadPipeline, Integer>, PadPipelineNumberingRepository {
-}
