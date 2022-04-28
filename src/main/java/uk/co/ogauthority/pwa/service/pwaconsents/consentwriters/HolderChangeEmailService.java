397b788cf808c0ba44486c330c255bd72161b5f7
Cade Ayres <cade.ayres@fivium.co.uk>
2022-04-28 17:27:52 +0100

    Holder change email is not issued when new portal org group is the same

--- src/main/java/uk/co/ogauthority/pwa/service/pwaconsents/consentwriters/HolderChangeEmailService.java
+++ src/main/java/uk/co/ogauthority/pwa/service/pwaconsents/consentwriters/HolderChangeEmailService.java
@@ -3,7 +3,9 @@ package uk.co.ogauthority.pwa.service.pwaconsents.consentwriters;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -59,12 +61,28 @@ public class HolderChangeEmailService {
     holderOuIds.addAll(holderOuIdsEnded);
     holderOuIds.addAll(holderOuIdsAdded);
 
-    var holderOrgUnits = portalOrganisationsAccessor.getOrganisationUnitsByIdIn(holderOuIds);
+    var addedOrgUnits = portalOrganisationsAccessor.getOrganisationUnitsByIdIn(holderOuIdsAdded);
+    var endedOrgUnits = portalOrganisationsAccessor.getOrganisationUnitsByIdIn(holderOuIdsEnded);
+    var holderOrgUnits = Stream.of(addedOrgUnits.stream(), endedOrgUnits.stream())
+        .flatMap(e -> e)
+        .collect(Collectors.toList());
 
     var ouIdsWithNoOrgGroupCsv = holderOrgUnits.stream()
         .filter(o -> o.getPortalOrganisationGroup().isEmpty())
         .map(o -> String.valueOf(o.getOuId()))
         .collect(Collectors.joining(","));
+
+    var addedOrgUnitPortalOrgGroupIds = getPortalOrganisationGroupIdsFromOrgUnits(addedOrgUnits);
+    var endedOrgUnitPortalOrgGroupIds = getPortalOrganisationGroupIdsFromOrgUnits(endedOrgUnits);
+
+    addedOrgUnitPortalOrgGroupIds.removeAll(endedOrgUnitPortalOrgGroupIds);
+
+    var primaryHolderChanged = addedOrgUnitPortalOrgGroupIds.size() > 0;
+
+    if (!primaryHolderChanged) {
+      LOGGER.info("Holder changed however belongs to same org group");
+      return;
+    }
 
     if (!StringUtils.isEmpty(ouIdsWithNoOrgGroupCsv)) {
       LOGGER.error("Found holder org units not associated with org groups and cannot email. paId={} ouIds={}",
@@ -105,6 +123,14 @@ public class HolderChangeEmailService {
 
     });
 
+  }
+
+  private Set<Integer> getPortalOrganisationGroupIdsFromOrgUnits(List<PortalOrganisationUnit> portalOrganisationUnits) {
+    return portalOrganisationUnits.stream()
+        .filter(portalOrganisationUnit -> portalOrganisationUnit.getPortalOrganisationGroup().isPresent())
+        .flatMap(portalOrganisationUnit -> portalOrganisationUnit.getPortalOrganisationGroup().stream())
+        .map(PortalOrganisationGroup::getOrgGrpId)
+        .collect(Collectors.toSet());
   }
 
   private String getHolderNameCsv(List<Integer> holderOuIds,
