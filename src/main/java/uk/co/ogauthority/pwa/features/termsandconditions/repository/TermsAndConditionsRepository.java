775a4b90e8f1565d988994cf4a78587da89de807
Alex McRae-Adams <alex.mcrae-adams@fivium.co.uk>
2023-07-06 16:31:28 +0100

    Added functionality to allow for T&C mail merge fields to be filled on consent documents.

--- src/main/java/uk/co/ogauthority/pwa/features/termsandconditions/repository/TermsAndConditionsRepository.java
+++ src/main/java/uk/co/ogauthority/pwa/features/termsandconditions/repository/TermsAndConditionsRepository.java
@@ -3,10 +3,9 @@ package uk.co.ogauthority.pwa.features.termsandconditions.repository;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 import uk.co.ogauthority.pwa.features.termsandconditions.model.PwaTermsAndConditions;
+import uk.co.ogauthority.pwa.model.entity.masterpwas.MasterPwa;
 
 @Repository
 public interface TermsAndConditionsRepository extends CrudRepository<PwaTermsAndConditions, Integer> {
-
-
-
+  public PwaTermsAndConditions getPwaTermsAndConditionsByMasterPwa(MasterPwa masterPwa);
 }
--- src/main/java/uk/co/ogauthority/pwa/service/mailmerge/TermsAndConditionsMailMergeResolver.java
+++ src/main/java/uk/co/ogauthority/pwa/service/mailmerge/TermsAndConditionsMailMergeResolver.java
@@ -4,19 +4,31 @@ import static uk.co.ogauthority.pwa.model.entity.enums.mailmerge.MailMergeFieldM
 import static uk.co.ogauthority.pwa.model.entity.enums.mailmerge.MailMergeFieldMnem.HUOO_TERMS;
 import static uk.co.ogauthority.pwa.model.entity.enums.mailmerge.MailMergeFieldMnem.VARIATION_TERM;
 
-import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 import org.springframework.stereotype.Service;
 import uk.co.ogauthority.pwa.domain.pwa.application.model.PwaApplication;
+import uk.co.ogauthority.pwa.features.termsandconditions.service.TermsAndConditionsService;
 import uk.co.ogauthority.pwa.model.entity.enums.mailmerge.MailMergeFieldMnem;
 import uk.co.ogauthority.pwa.service.documents.DocumentSource;
 import uk.co.ogauthority.pwa.service.documents.templates.TemplateDocumentSource;
+import uk.co.ogauthority.pwa.service.pwaapplications.PwaApplicationDetailService;
 
 @Service
 public class TermsAndConditionsMailMergeResolver implements DocumentSourceMailMergeResolver {
+
+  private final PwaApplicationDetailService pwaApplicationDetailService;
+
+  private final TermsAndConditionsService termsAndConditionsService;
   private List<Class> supportedClasses = List.of(PwaApplication.class, TemplateDocumentSource.class);
+
+  public TermsAndConditionsMailMergeResolver(PwaApplicationDetailService pwaApplicationDetailService,
+                                             TermsAndConditionsService termsAndConditionsService) {
+    this.pwaApplicationDetailService = pwaApplicationDetailService;
+    this.termsAndConditionsService = termsAndConditionsService;
+  }
 
   @Override
   public boolean supportsDocumentSource(DocumentSource documentSource) {
@@ -34,6 +46,31 @@ public class TermsAndConditionsMailMergeResolver implements DocumentSourceMailMe
 
   @Override
   public Map<String, String> resolveMergeFields(DocumentSource documentSource) {
-    return Collections.emptyMap();
+    var resolvedFields = new HashMap<String, String>();
+    if (documentSource instanceof PwaApplication) {
+      var app = (PwaApplication) documentSource;
+      var termsAndConditions = termsAndConditionsService.getByMasterPwa(app.getMasterPwa());
+      for (var field : getAvailableMailMergeFields(documentSource)) {
+        switch (field) {
+          case VARIATION_TERM:
+            resolvedFields.put(VARIATION_TERM.name(), String.valueOf(termsAndConditions.getVariationTerm()));
+            break;
+          case HUOO_TERMS:
+            resolvedFields.put(HUOO_TERMS.name(), termsAndConditions.getHuooTerms());
+            break;
+          case DEPCON_TERMS:
+            resolvedFields.put(DEPCON_TERMS.name(),
+                  String.valueOf(termsAndConditions.getDepconParagraph() + termsAndConditions.getDepconSchedule()));
+            break;
+          default:
+            break;
+          }
+      }
+    } else {
+      for (var field : getAvailableMailMergeFields(documentSource)) {
+        resolvedFields.put(field.name(), "??" + field.name() + "??");
+      }
+    }
+    return resolvedFields;
   }
 }
