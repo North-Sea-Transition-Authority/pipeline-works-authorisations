60b2b1c607419b4ee3896f2ce3d8ad0afd5491d7
Alex McRae-Adams <alex.mcrae-adams@fivium.co.uk>
2023-07-21 10:16:30 +0100

    Added Project Information Licences Entity

---
+++ src/main/java/uk/co/ogauthority/pwa/features/application/tasks/projectinfo/PadProjectInformationLicenseReferences.java
@@ -0,0 +1,62 @@
+package uk.co.ogauthority.pwa.features.application.tasks.projectinfo;
+
+import java.time.Instant;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import uk.co.ogauthority.pwa.integrations.energyportal.pearslicensing.external.PearsLicence;
+
+@Entity(name = "PAD_PROJECT_INFORMATION_LICENCE")
+public class PadProjectInformationLicenseReferences {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Integer id;
+
+  @ManyToOne
+  @JoinColumn(name = "PAD_PROJECT_INFORMATION_ID")
+  private PadProjectInformation padProjectInformation;
+
+  @ManyToOne
+  @JoinColumn(name = "PEARS_LICENSE_NUMBER")
+  private PearsLicence pearsLicence;
+
+  private Instant createdTimestamp;
+
+  public Integer getId() {
+    return id;
+  }
+
+  public void setId(Integer id) {
+    this.id = id;
+  }
+
+  public PadProjectInformation getPadProjectInformation() {
+    return padProjectInformation;
+  }
+
+  public void setPadProjectInformation(
+      PadProjectInformation padProjectInformation) {
+    this.padProjectInformation = padProjectInformation;
+  }
+
+  public PearsLicence getPearsLicence() {
+    return pearsLicence;
+  }
+
+  public void setPearsLicence(
+      PearsLicence pearsLicence) {
+    this.pearsLicence = pearsLicence;
+  }
+
+  public Instant getCreatedTimestamp() {
+    return createdTimestamp;
+  }
+
+  public void setCreatedTimestamp(Instant createdTimestamp) {
+    this.createdTimestamp = createdTimestamp;
+  }
+}
