4bdc807081eb82ee558c4d03ac89af3b47f1ad3a
Alex McRae-Adams <alex.mcrae-adams@fivium.co.uk>
2023-07-31 15:22:46 +0100

    Fixed checkstyle

--- src/main/java/uk/co/ogauthority/pwa/features/application/tasks/projectinfo/PadProjectInformationLicenceApplicationsRepository.java
+++ src/main/java/uk/co/ogauthority/pwa/features/application/tasks/projectinfo/PadProjectInformationLicenceApplicationsRepository.java
@@ -3,7 +3,8 @@ package uk.co.ogauthority.pwa.features.application.tasks.projectinfo;
 import java.util.List;
 import org.springframework.data.repository.CrudRepository;
 
-public interface PadProjectInformationLicenceApplicationsRepository extends CrudRepository<PadProjectInformationLicenceApplications, Integer> {
+public interface PadProjectInformationLicenceApplicationsRepository
+    extends CrudRepository<PadProjectInformationLicenceApplications, Integer> {
   List<PadProjectInformationLicenceApplications> findAllByPadProjectInformation(PadProjectInformation padProjectInformation);
 
   void deleteAllByPadProjectInformation(PadProjectInformation projectInformation);
