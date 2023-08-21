a1b4b8ff5a7b3de4175dce53d662095c99e69f09
Alex McRae-Adams <alex.mcrae-adams@fivium.co.uk>
2023-08-21 09:49:45 +0100

    Reverted unnecessary change.

--- src/main/java/uk/co/ogauthority/pwa/features/termsandconditions/controller/TermsAndConditionsManagementController.java
+++ src/main/java/uk/co/ogauthority/pwa/features/termsandconditions/controller/TermsAndConditionsManagementController.java
@@ -41,7 +41,8 @@ public class TermsAndConditionsManagementController {
         .addObject("termsAndConditionsFormUrl", ReverseRouter.route(on(TermsAndConditionsFormController.class)
             .renderNewTermsAndConditionsForm(null, user)))
         .addObject("clearFilterUrl", ReverseRouter.route(on(TermsAndConditionsManagementController.class)
-            .renderTermsAndConditionsManagement(new TermsAndConditionsFilterForm(), null, null)));
+            .renderTermsAndConditionsManagement(new TermsAndConditionsFilterForm(), null, null)))
+        .addObject("form", form);
   }
 
   @PostMapping
@@ -54,7 +55,7 @@ public class TermsAndConditionsManagementController {
     paramMap.setAll(FormObjectMapper.toMap(form));
 
     return ReverseRouter.redirectWithQueryParamMap(on(TermsAndConditionsManagementController.class)
-       .renderTermsAndConditionsManagement(null, null, null), paramMap);
+       .renderTermsAndConditionsManagement(form, null, null), paramMap);
   }
 
   private void checkUserPrivilege(AuthenticatedUserAccount authenticatedUser) {
