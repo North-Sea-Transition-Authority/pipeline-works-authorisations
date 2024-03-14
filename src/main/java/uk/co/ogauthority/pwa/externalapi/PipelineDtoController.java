37d6f5aaf71c5275deda42a10bac3e75cc0f2c82
lauren little <lauren.little@fivium.co.uk>
2024-03-14 16:07:35 +0000

    fixed checkstyle

--- src/main/java/uk/co/ogauthority/pwa/externalapi/PipelineDtoController.java
+++ src/main/java/uk/co/ogauthority/pwa/externalapi/PipelineDtoController.java
@@ -23,14 +23,15 @@ public class PipelineDtoController {
   }
 
   /**
-    * @deprecated This is currently consumed by EPA & removing it will cause their APIs to break. Instead, it has been
-    * deprecated and will be removed after EPA no longer consumes it.
-    * @param ids pipeline ids
-    * @param pipelineNumber pipeline number
-    * @param pwaIds pwa ids
-    * @param pwaReference pwa reference
-    * @return PipelineDto
-   */
+   * This method searches for pipelines and their associated pwas.
+   * @deprecated This is currently consumed by EPA & removing it will cause their APIs to break. Instead, it has been
+                  deprecated and will be removed after EPA no longer consumes it.
+   * @param ids pipeline ids
+   * @param pipelineNumber pipeline number
+   * @param pwaIds pwa ids
+   * @param pwaReference pwa reference
+   * @return PipelineDto
+  */
   @Deprecated
   @GetMapping("/pipelines-deprecated")
   List<PipelineDto> searchPipelines(@RequestParam(name = "ids", required = false) List<Integer> ids,
--- src/main/java/uk/co/ogauthority/pwa/externalapi/PipelineDtoRepository.java
+++ src/main/java/uk/co/ogauthority/pwa/externalapi/PipelineDtoRepository.java
@@ -10,8 +10,9 @@ import uk.co.ogauthority.pwa.model.entity.pipelines.PipelineDetail;
 interface PipelineDtoRepository extends CrudRepository<PipelineDetail, Integer> {
 
   /**
+    * This method searches for pipelines and their associated pwas.
     * @deprecated This is currently consumed by EPA & removing it will cause their APIs to break. Instead, it has been
-    * deprecated and will be removed after EPA no longer consumes it.
+                  deprecated and will be removed after EPA no longer consumes it.
     * @param ids pipeline ids
     * @param pipelineNumber pipeline number
     * @param pwaIds pwa ids
