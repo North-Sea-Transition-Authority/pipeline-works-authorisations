14e8537482aab20e37f5efa81b289a46b6360450
lauren little <lauren.little@fivium.co.uk>
2024-09-23 09:49:32 +0100

    added tests

--- src/main/java/uk/co/ogauthority/pwa/externalapi/PwaConsentDtoController.java
+++ src/main/java/uk/co/ogauthority/pwa/externalapi/PwaConsentDtoController.java
@@ -5,14 +5,11 @@ import static uk.co.ogauthority.pwa.externalapi.PwaConsentDtoController.ENERGY_P
 import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
-import org.apache.commons.collections4.CollectionUtils;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.server.ResponseStatusException;
 
 @RestController
 @RequestMapping(ENERGY_PORTAL_API_BASE_PATH)
@@ -29,10 +26,6 @@ public class PwaConsentDtoController {
 
   @GetMapping("/consents")
   List<PwaConsentDto> searchPwaConsents(@RequestParam(name = "pwaIds") List<Integer> pwaIds) {
-
-    if (CollectionUtils.isEmpty(pwaIds)) {
-      throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "At least one request parameter must be non-null");
-    }
 
     return repository.searchPwaConsents(pwaIds)
         .stream()
