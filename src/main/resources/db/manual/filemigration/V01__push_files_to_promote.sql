6ee94902a82f9d5c75ec9d376d60e95419d62522
Ryan <34923968+ZenoxSphere@users.noreply.github.com>
2025-05-06 15:20:09 +0100

    PWARE-102: migrate generated consent documents to S3 (#97)
    * PWARE-102: migrate generated consent documents to S3
    
    * PWARE-102: pr comments

--- src/main/resources/db/manual/filemigration/V01__push_files_to_promote.sql
+++ src/main/resources/db/manual/filemigration/V01__push_files_to_promote.sql
@@ -37,3 +37,46 @@ FROM pwa.app_files af
     LEFT JOIN pwa.uploaded_files uf on af.file_id = uf.file_id
 WHERE af.file_id IS NOT NULL
     AND uf.file_data IS NOT NULL;
+
+INSERT INTO promotemgr.s3_file_migration (
+    fox_file_id,
+    application,
+    reference,
+    directory,
+    filename,
+    content
+)
+SELECT
+    dr.id,
+    'PWA' application,
+    pa.id reference,
+    'migrated' directory,
+    REPLACE(pa.consent_reference, '/', '-')  || ' consent document.pdf' filename,
+    dr.generated_doc content
+FROM pwa.docgen_runs dr
+    JOIN pwa.document_instances di ON dr.di_id = di.id
+    JOIN pwa.pwa_applications pa ON di.pwa_application_id = pa.id
+WHERE dr.docgen_type = 'FULL'
+    AND dr.status = 'COMPLETE';
+
+INSERT INTO promotemgr.s3_file_migration (
+    fox_file_id,
+    application,
+    reference,
+    directory,
+    filename,
+    content
+)
+SELECT
+    dr.id,
+    'PWA' application,
+    pa.id reference,
+    'migrated' directory,
+    REPLACE(pa.app_reference, '/', '-')  || ' consent preview' || dr.ID || '.pdf' filename,
+    dr.generated_doc content
+FROM pwa.docgen_runs dr
+    JOIN pwa.document_instances di ON dr.di_id = di.id
+    JOIN pwa.pwa_applications pa ON di.pwa_application_id = pa.id
+WHERE dr.docgen_type = 'PREVIEW'
+    AND dr.status = 'COMPLETE'
+    AND dr.generated_doc IS NOT NULL;
--- src/main/resources/db/manual/filemigration/V02__pull_file_data_into_uploaded_files.sql
+++ src/main/resources/db/manual/filemigration/V02__pull_file_data_into_uploaded_files.sql
@@ -71,4 +71,72 @@ FROM
         LEFT JOIN promotemgr.s3_file_migration sfm ON sfm.fox_file_id = uf.file_id AND sfm.reference = af.pa_id
 WHERE sfm.migrated_timestamp IS NOT NULL;
 
+INSERT INTO pwa.FILE_UPLOAD_LIBRARY_UPLOADED_FILES (
+    id,
+    bucket,
+    key,
+    name,
+    content_type,
+    content_length,
+    uploaded_at,
+    usage_id,
+    usage_type,
+    document_type,
+    description,
+    uploaded_by
+)
+SELECT
+    hextoraw(REPLACE(random_uuid(), '-', '')),
+    sfm.s3_bucket bucket,
+    sfm.s3_path key,
+    sfm.filename name,
+    'application/pdf' content_type,
+    dbms_lob.getlength(dr.generated_doc),
+    dr.completed_on,
+    pa.id,
+    'PwaApplication',
+    'CONSENT_DOCUMENT',
+    '',
+    dr.SCHEDULED_BY_PERSON_ID
+FROM pwa.docgen_runs dr
+    JOIN pwa.document_instances di ON dr.di_id = di.id
+    JOIN pwa.pwa_applications pa ON di.pwa_application_id = pa.id
+    JOIN promotemgr.s3_file_migration sfm ON sfm.fox_file_id = dr.id AND sfm.reference = pa.id
+WHERE dr.docgen_type = 'FULL'
+    AND sfm.migrated_timestamp IS NOT NULL;
+
+INSERT INTO pwa.FILE_UPLOAD_LIBRARY_UPLOADED_FILES (
+    id,
+    bucket,
+    key,
+    name,
+    content_type,
+    content_length,
+    uploaded_at,
+    usage_id,
+    usage_type,
+    document_type,
+    description,
+    uploaded_by
+)
+SELECT
+    hextoraw(REPLACE(random_uuid(), '-', '')),
+    sfm.s3_bucket bucket,
+    sfm.s3_path key,
+    sfm.filename name,
+    'application/pdf' content_type,
+    dbms_lob.getlength(dr.generated_doc),
+    dr.completed_on,
+    pa.id,
+    'PwaApplication',
+    'CONSENT_PREVIEW',
+    '',
+    dr.scheduled_by_person_id
+FROM pwa.docgen_runs dr
+    JOIN pwa.document_instances di ON dr.di_id = di.id
+    JOIN pwa.pwa_applications pa ON di.pwa_application_id = pa.id
+    JOIN promotemgr.s3_file_migration sfm ON sfm.fox_file_id = dr.id AND sfm.reference = pa.id
+WHERE dr.docgen_type = 'PREVIEW'
+  AND sfm.migrated_timestamp IS NOT NULL;
+
 END;
