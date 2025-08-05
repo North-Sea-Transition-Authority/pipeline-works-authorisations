5d381a4c1dabe3b83dce17528caf64f8f7a4245d
Ryan <34923968+ZenoxSphere@users.noreply.github.com>
2025-08-05 13:56:35 +0100

    fix issue where multiple pad files are overwritten with one file id (#178)

--- src/main/resources/db/manual/filemigration/V03__replace_old_file_ids.sql
+++ src/main/resources/db/manual/filemigration/V03__replace_old_file_ids.sql
@@ -1,18 +1,18 @@
 BEGIN
 
     FOR file_map_rec IN (
-        SELECT fim.old_file_id, fim.new_file_id
+        SELECT fim.old_file_id, fim.new_file_id, fim.usage_id
         FROM pwa.file_id_map fim
     )
     LOOP
 
         UPDATE pwa.pad_files
         SET file_id = file_map_rec.new_file_id
-        WHERE file_id = file_map_rec.old_file_id;
+        WHERE file_id = file_map_rec.old_file_id AND pad_id = file_map_rec.usage_id;
 
         UPDATE pwa.app_files
         SET file_id = file_map_rec.new_file_id
-        WHERE file_id = file_map_rec.old_file_id;
+        WHERE file_id = file_map_rec.old_file_id AND pa_id = file_map_rec.usage_id;
 
     END LOOP;
 
