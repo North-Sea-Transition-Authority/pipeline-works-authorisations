eaa7346ff01791f1c598e63c9eabe9f483c11605
Ryan <34923968+ZenoxSphere@users.noreply.github.com>
2025-09-16 13:47:59 +0100

    Fix bug where two different files with the exact same name and extension cause an exception to be thrown (#195)

--- src/main/java/uk/co/ogauthority/pwa/features/application/tasks/permdeposit/PermanentDepositService.java
+++ src/main/java/uk/co/ogauthority/pwa/features/application/tasks/permdeposit/PermanentDepositService.java
@@ -12,6 +12,7 @@ import java.util.Map;
 import java.util.Optional;
 import java.util.function.Function;
 import java.util.stream.Collectors;
+import org.apache.commons.lang3.tuple.Pair;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,7 +21,6 @@ import org.springframework.stereotype.Service;
 import org.springframework.validation.BeanPropertyBindingResult;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.beanvalidation.SpringValidatorAdapter;
-import uk.co.fivium.fileuploadlibrary.core.UploadedFile;
 import uk.co.ogauthority.pwa.domain.pwa.application.model.PwaApplicationType;
 import uk.co.ogauthority.pwa.domain.pwa.pipeline.model.PhysicalPipelineState;
 import uk.co.ogauthority.pwa.domain.pwa.pipeline.model.PipelineId;
@@ -450,10 +450,16 @@ public class PermanentDepositService implements ApplicationFormSectionService {
     padFileManagementService.copyUploadedFiles(fromDetail, toDetail, FileDocumentType.DEPOSIT_DRAWINGS);
 
     var fromFiles = padFileManagementService.getUploadedFiles(fromDetail, FileDocumentType.DEPOSIT_DRAWINGS).stream()
-        .collect(Collectors.toMap(UploadedFile::getName, Function.identity()));
+        .collect(
+            Collectors.toMap(file -> Pair.of(file.getName(), file.getUploadedAt()),
+            Function.identity())
+        );
 
     var toFiles = padFileManagementService.getUploadedFiles(toDetail, FileDocumentType.DEPOSIT_DRAWINGS).stream()
-        .collect(Collectors.toMap(UploadedFile::getName, Function.identity()));
+        .collect(
+            Collectors.toMap(file -> Pair.of(file.getName(), file.getUploadedAt()),
+            Function.identity())
+        );
 
     var originalToCopiedFileIDs = fromFiles.entrySet().stream()
         .collect(Collectors.toMap(e -> e.getValue().getId(), e -> toFiles.get(e.getKey()).getId()));
